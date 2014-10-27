791ebc0a45ad51297543e7a55999140da468cc5d
chylex <info@chylex.com>
2014-10-27 02:49:52 +0100

    Moar populators :3

---
+++ src/main/java/chylex/hee/world/feature/blobs/populators/BlobPopulatorLake.java
@@ -0,0 +1,52 @@
+package chylex.hee.world.feature.blobs.populators;
+import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import chylex.hee.world.feature.blobs.BlobPopulator;
+import chylex.hee.world.feature.util.DecoratorFeatureGenerator;
+
+public class BlobPopulatorLake extends BlobPopulator{
+	private Block liquid;
+	private double minRadius, maxRadius;
+	
+	public BlobPopulatorLake(int weight){
+		super(weight);
+	}
+	
+	public BlobPopulatorLake block(Block liquid){
+		this.liquid = liquid;
+		return this;
+	}
+	
+	public BlobPopulatorLake rad(double minRadius, double maxRadius){
+		this.minRadius = minRadius;
+		this.maxRadius = maxRadius;
+		return this;
+	}
+
+	@Override
+	public void generate(DecoratorFeatureGenerator gen, Random rand){
+		double rad = minRadius+rand.nextDouble()*(maxRadius-minRadius);
+		int irad = (int)Math.ceil(rad);
+		
+		for(int attempt = 0, x, y, z; attempt < 30; attempt++){
+			x = rand.nextInt(32)-16;
+			y = rand.nextInt(32)-16;
+			z = rand.nextInt(32)-16;
+			
+			if (gen.getBlock(x,y,z) == Blocks.end_stone && gen.getBlock(x,y+1,z) == Blocks.air){
+				for(int xx = x-irad; xx <= x+irad; xx++){
+					for(int zz = z-irad; zz <= z+irad; zz++){
+						if (gen.getBlock(xx,y,zz) == Blocks.end_stone && gen.getBlock(xx,y+1,zz) == Blocks.air &&
+							gen.getBlock(xx-1,y,zz) == Blocks.air && gen.getBlock(xx+1,y,zz) == Blocks.air &&
+							gen.getBlock(xx,y,zz-1) == Blocks.air && gen.getBlock(xx,y,zz+1) == Blocks.air){
+							gen.setBlock(xx,y,zz,liquid);
+						}
+					}
+				}
+				
+				break;
+			}
+		}
+	}
+}
---
+++ src/main/java/chylex/hee/world/feature/blobs/populators/BlobPopulatorPlant.java
@@ -0,0 +1,74 @@
+package chylex.hee.world.feature.blobs.populators;
+import java.util.List;
+import java.util.Random;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import chylex.hee.world.feature.blobs.BlobPopulator;
+import chylex.hee.world.feature.util.DecoratorFeatureGenerator;
+import chylex.hee.world.util.BlockLocation;
+import chylex.hee.world.util.IRandomAmount;
+
+public class BlobPopulatorPlant extends BlobPopulator{
+	private Block plant;
+	private IRandomAmount amountGen;
+	private int minAttempts, maxAttempts, minPlantAmount, maxPlantAmount;
+	private boolean knownBlockLocations;
+	
+	public BlobPopulatorPlant(int weight){
+		super(weight);
+	}
+	
+	public BlobPopulatorPlant block(Block plant){
+		this.plant = plant;
+		return this;
+	}
+	
+	public BlobPopulatorPlant attempts(int minAttempts, int maxAttempts){
+		this.minAttempts = (byte)minAttempts;
+		this.maxAttempts = (byte)maxAttempts;
+		return this;
+	}
+	
+	public BlobPopulatorPlant blockAmount(IRandomAmount amountGen, int minPlantAmount, int maxPlantAmount){
+		this.amountGen = amountGen;
+		this.minPlantAmount = (byte)minPlantAmount;
+		this.maxPlantAmount = (byte)maxPlantAmount;
+		return this;
+	}
+	
+	/**
+	 * Populator will know all generated blocks and use those to place ores, attempt amount will have an effect in cases when the random block is not air above End Stone.
+	 */
+	public BlobPopulatorPlant knownBlockLocations(){
+		this.knownBlockLocations = true;
+		return this;
+	}
+
+	@Override
+	public void generate(DecoratorFeatureGenerator gen, Random rand){
+		int blocks = amountGen.generate(rand,minPlantAmount,maxPlantAmount);
+		List<BlockLocation> locs = null;
+		
+		for(int attempt = 0, attempts = minAttempts+rand.nextInt(maxAttempts-minAttempts+1), x, y, z; attempt < attempts && blocks > 0; attempt++){
+			if (knownBlockLocations){
+				if (attempt == 0)locs = gen.getUsedLocations();
+				if (locs.isEmpty())return;
+				
+				BlockLocation loc = locs.get(rand.nextInt(locs.size()));
+				x = loc.x;
+				y = loc.y;
+				z = loc.z;
+			}
+			else{
+				x = rand.nextInt(32)-16;
+				y = rand.nextInt(32)-16;
+				z = rand.nextInt(32)-16;
+			}
+			
+			if (gen.getBlock(x,y,z) == Blocks.air && gen.getBlock(x,y-1,z) == Blocks.end_stone){
+				gen.setBlock(x,y,z,plant);
+				--blocks;
+			}
+		}
+	}
+}
