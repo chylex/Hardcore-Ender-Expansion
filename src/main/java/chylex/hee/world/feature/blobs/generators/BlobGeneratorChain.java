ce5db8858a889281328a5b832f7e7f94d8bd54d9
chylex <info@chylex.com>
2014-11-10 17:33:40 +0100

    Github, you forgot this file >.>

--- src/main/java/chylex/hee/world/feature/blobs/generators/BlobGeneratorChain.java
+++ src/main/java/chylex/hee/world/feature/blobs/generators/BlobGeneratorChain.java
@@ -10,6 +10,7 @@ public class BlobGeneratorChain extends BlobGenerator{
 	private IRandomAmount amountGen = IRandomAmount.exact;
 	private byte minAmount, maxAmount;
 	private double minRad, maxRad, minChainDistMp, maxChainDistMp;
+	private boolean unifiedSize;
 	
 	public BlobGeneratorChain(int weight){
 		super(weight);
@@ -34,13 +35,21 @@ public class BlobGeneratorChain extends BlobGenerator{
 		return this;
 	}
 	
+	/**
+	 * Makes each blob the same size.
+	 */
+	public BlobGeneratorChain unifySize(){
+		this.unifiedSize = true;
+		return this;
+	}
+	
 	@Override
 	public void generate(DecoratorFeatureGenerator gen, Random rand){
 		double prevRad = 0D;
 		double x = 0D, y = 0D, z = 0D;
 		
 		for(int a = 0, amount = amountGen.generate(rand,minAmount,maxAmount); a < amount; a++){
-			double rad = minRad+rand.nextDouble()*(maxRad-minRad);
+			double rad = unifiedSize && a > 0 ? prevRad : minRad+rand.nextDouble()*(maxRad-minRad);
 			
 			Vec3 vec = a == 0 ? Vec3.createVectorHelper(0D,0D,0D) : DragonUtil.getRandomVector(rand);
 			double dist = a == 0 ? 0D : prevRad*(minChainDistMp+rand.nextDouble()*(maxChainDistMp-minChainDistMp));
