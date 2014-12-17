8b306da3e4087e2f2d90148f30b9890805bf47b5
chylex <chylex@fo2.cz>
2014-12-17 13:20:07 +0100

    Fix worldgen file

--- src/main/java/chylex/hee/system/savedata/types/WorldGenSavefile.java
+++ src/main/java/chylex/hee/system/savedata/types/WorldGenSavefile.java
@@ -27,12 +27,14 @@ public class WorldGenSavefile extends WorldSavefile{
 	}
 	
 	public void addElementAt(int chunkX, int chunkZ, WorldGenElement element){
-		elements.put(getFromChunkPos(chunkX,chunkZ),element);
+		elements.put(getFromChunkPos(chunkX,chunkZ),element);System.out.println("adding "+getFromChunkPos(chunkX,chunkZ));
 		setModified();
 	}
 	
 	public boolean isAreaBlocked(int chunkX, int chunkZ, WorldGenElement element){
-		for(TLongObjectIterator<WorldGenElement> iter = elements.iterator(); iter.hasNext(); iter.advance()){
+		for(TLongObjectIterator<WorldGenElement> iter = elements.iterator(); iter.hasNext();){
+			iter.advance();
+			
 			if (Math.abs(getX(iter.key())-chunkX) <= iter.value().rad+element.rad && Math.abs(getZ(iter.key())-chunkZ) <= iter.value().rad+element.rad){
 				return true;
 			}
@@ -42,16 +44,16 @@ public class WorldGenSavefile extends WorldSavefile{
 	}
 	
 	private final long getFromChunkPos(int x, int z){
-		if (x >= -1875000 && z >= -1875000 && x < 1875000 && z < 1875000)return ((x+1875000)<<24)|(z+1875000);
+		if (x >= -1875000 && z >= -1875000 && x < 1875000 && z < 1875000)return ((x+1875000L)<<24L)|(z+1875000L);
 		else return Long.MAX_VALUE;
 	}
 	
 	private final int getX(long pos){
-		return (int)((pos>>24)-1875000);
+		return (int)((pos>>24L)-1875000L);
 	}
 	
 	private final int getZ(long pos){
-		return (int)((pos&16777215)-1875000);
+		return (int)((pos&16777215L)-1875000L);
 	}
 
 	@Override
@@ -59,14 +61,16 @@ public class WorldGenSavefile extends WorldSavefile{
 		TByteObjectHashMap<NBTTagList> saved = new TByteObjectHashMap<>();
 		byte ord;
 		
-		for(TLongObjectIterator<WorldGenElement> iter = elements.iterator(); iter.hasNext(); iter.advance()){
+		for(TLongObjectIterator<WorldGenElement> iter = elements.iterator(); iter.hasNext();){
+			iter.advance();
 			ord = (byte)iter.value().ordinal();
 			
 			if (!saved.containsKey(ord))saved.put(ord,new NBTTagList());
 			saved.get(ord).appendTag(new NBTTagDouble(Double.longBitsToDouble(iter.key())));
 		}
 		
-		for(TByteObjectIterator<NBTTagList> iter = saved.iterator(); iter.hasNext(); iter.advance()){
+		for(TByteObjectIterator<NBTTagList> iter = saved.iterator(); iter.hasNext();){
+			iter.advance();
 			nbt.setTag(String.valueOf(iter.key()),iter.value());
 		}
 	}
