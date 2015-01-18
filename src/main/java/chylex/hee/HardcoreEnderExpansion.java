f11923a7cf9df7eee734dc61c7b8619f6656de60
chylex <chylex@fo2.cz>
2015-01-18 01:30:56 +0100

    Push 1.7 update

--- src/main/java/chylex/hee/HardcoreEnderExpansion.java
+++ src/main/java/chylex/hee/HardcoreEnderExpansion.java
@@ -139,7 +139,7 @@ public class HardcoreEnderExpansion{
 	@SidedProxy(clientSide = "chylex.hee.proxy.NotificationClientProxy", serverSide = "chylex.hee.proxy.NotificationCommonProxy")
 	public static NotificationCommonProxy notifications;
 	
-	public static final int buildId = 19_12_14_0;
+	public static final int buildId = 18_91_15_0;
 	
 	public static String modVersion;
 	public static String configPath;
