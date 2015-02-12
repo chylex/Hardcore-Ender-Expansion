abb2ff1fcc135dfc5204d2ee9755edf73a874e39
chylex <info@chylex.com>
2015-02-12 09:39:57 +0100

    Removed old enhancement loading

--- src/main/java/chylex/hee/mechanics/enhancements/types/EnderPearlEnhancements.java
+++ src/main/java/chylex/hee/mechanics/enhancements/types/EnderPearlEnhancements.java
@@ -1,15 +1,9 @@
 package chylex.hee.mechanics.enhancements.types;
-import java.util.ArrayList;
-import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.common.util.Constants;
-import chylex.hee.item.ItemList;
 import chylex.hee.mechanics.enhancements.EnhancementEnumHelper;
 import chylex.hee.mechanics.enhancements.EnhancementHandler;
 import chylex.hee.mechanics.enhancements.IEnhancementEnum;
@@ -18,43 +12,18 @@ import chylex.hee.system.util.IItemSelector.IRepresentativeItemSelector;
 import chylex.hee.system.util.IItemSelector.IRepresentativeItemSelector.SimpleItemSelector;
 
 public enum EnderPearlEnhancements implements IEnhancementEnum{
-	NO_FALL_DAMAGE("No fall damage", new SimpleItemSelector(Blocks.wool)),
-	NO_GRAVITY("No gravity", new SimpleItemSelector(Items.feather)),
-	INCREASED_RANGE("Increased range", new SimpleItemSelector(Items.string)),
-	DOUBLE_SPEED("Double speed", new SimpleItemSelector(Items.gold_nugget)),
-	EXPLOSIVE("Explosive", new SimpleItemSelector(Items.gunpowder)),
-	FREEZE("Freeze", new SimpleItemSelector(Items.snowball)),
-	RIDING("Riding", new SimpleItemSelector(Items.lead));
-	
-	public static List<EnderPearlEnhancements> getOldEnhancements(ItemStack is){
-		List<EnderPearlEnhancements> list = new ArrayList<>();
-		if (is.getItem() == Items.ender_pearl && is.getItem() != ItemList.enhanced_ender_pearl)return list;
-		
-		NBTTagCompound tag = is.stackTagCompound;
-		if (tag == null || !tag.hasKey("display"))return list;
-
-		NBTTagList lore = tag.getCompoundTag("display").getTagList("Lore",Constants.NBT.TAG_STRING);
-		if (lore == null)return list;
-		
-		for(int a = 0; a < lore.tagCount(); a++){
-			String line = lore.getStringTagAt(a);
-			for(EnderPearlEnhancements pearlType:values()){
-				if (line.endsWith(pearlType.oldLore)){
-					list.add(pearlType);
-					break;
-				}
-			}
-		}
-		
-		return list;
-	}
+	NO_FALL_DAMAGE(new SimpleItemSelector(Blocks.wool)),
+	NO_GRAVITY(new SimpleItemSelector(Items.feather)),
+	INCREASED_RANGE(new SimpleItemSelector(Items.string)),
+	DOUBLE_SPEED(new SimpleItemSelector(Items.gold_nugget)),
+	EXPLOSIVE(new SimpleItemSelector(Items.gunpowder)),
+	FREEZE(new SimpleItemSelector(Items.snowball)),
+	RIDING(new SimpleItemSelector(Items.lead));
 	
 	private final String name;
-	private String oldLore;
 	private IRepresentativeItemSelector itemSelector;
 	
-	private EnderPearlEnhancements(String lore, IRepresentativeItemSelector itemSelector){
-		this.oldLore = EnumChatFormatting.RESET.toString()+EnumChatFormatting.RED.toString()+lore;
+	private EnderPearlEnhancements(IRepresentativeItemSelector itemSelector){
 		this.name = EnhancementEnumHelper.getName(this,EnumChatFormatting.RED);
 		this.itemSelector = itemSelector;
 	}
--- src/main/java/chylex/hee/mechanics/enhancements/types/TransferenceGemEnhancements.java
+++ src/main/java/chylex/hee/mechanics/enhancements/types/TransferenceGemEnhancements.java
@@ -1,16 +1,10 @@
 package chylex.hee.mechanics.enhancements.types;
-import java.util.ArrayList;
-import java.util.List;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.EnumChatFormatting;
-import net.minecraftforge.common.util.Constants;
-import chylex.hee.item.ItemList;
 import chylex.hee.mechanics.brewing.PotionTypes;
 import chylex.hee.mechanics.enhancements.EnhancementEnumHelper;
 import chylex.hee.mechanics.enhancements.IEnhancementEnum;
@@ -18,7 +12,7 @@ import chylex.hee.system.util.IItemSelector.IRepresentativeItemSelector;
 import chylex.hee.system.util.IItemSelector.IRepresentativeItemSelector.SimpleItemSelector;
 
 public enum TransferenceGemEnhancements implements IEnhancementEnum{
-	HEAL("Heal",new IRepresentativeItemSelector(){
+	HEAL(new IRepresentativeItemSelector(){
 		@Override
 		public boolean isValid(ItemStack is){
 			if (is.getItem() != Items.potionitem)return false;
@@ -32,40 +26,14 @@ public enum TransferenceGemEnhancements implements IEnhancementEnum{
 			return PotionTypes.setCustomPotionEffect(new ItemStack(Items.potionitem,1,8197),new PotionEffect(Potion.heal.id,0,3));
 		}
 	}),
-	TOUCH("Touch", new SimpleItemSelector(Items.item_frame)),
-	MOB("Mob", new SimpleItemSelector(Items.lead));
-	
-	public static List<TransferenceGemEnhancements> getOldEnhancements(ItemStack is){
-		List<TransferenceGemEnhancements> list = new ArrayList<>();
-		if (is.getItem() != ItemList.transference_gem)return list;
-		
-		NBTTagCompound tag = is.stackTagCompound;
-		if (tag == null)return list;
-		if (!tag.hasKey("display"))return list;
-
-		NBTTagList lore = tag.getCompoundTag("display").getTagList("Lore",Constants.NBT.TAG_STRING);
-		if (lore == null)return list;
-		
-		for(int a = 0; a < lore.tagCount(); a++){
-			String line = lore.getStringTagAt(a);
-			for(TransferenceGemEnhancements enhancement:values()){
-				if (line.endsWith(enhancement.effectName)){
-					list.add(enhancement);
-					break;
-				}
-			}
-		}
-		
-		return list;
-	}
+	TOUCH(new SimpleItemSelector(Items.item_frame)),
+	MOB(new SimpleItemSelector(Items.lead));
 	
 	private final String name;
-	private final String effectName;
 	public final IRepresentativeItemSelector itemSelector;
 	
-	private TransferenceGemEnhancements(String effectName, IRepresentativeItemSelector itemSelector){
+	private TransferenceGemEnhancements(IRepresentativeItemSelector itemSelector){
 		this.name = EnhancementEnumHelper.getName(this,EnumChatFormatting.GRAY);
-		this.effectName = EnumChatFormatting.GRAY+effectName;
 		this.itemSelector = itemSelector;
 	}
 
