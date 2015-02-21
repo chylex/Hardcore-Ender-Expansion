c34069a50e8c29651bf4de52f0fa486a11d9c273
chylex <chylex@fo2.cz>
2015-02-21 12:21:26 +0100

    Fixed enderman biome replacement and their reg text

--- src/main/java/chylex/hee/world/biome/BiomeGenHardcoreEnd.java
+++ src/main/java/chylex/hee/world/biome/BiomeGenHardcoreEnd.java
@@ -4,12 +4,9 @@ import net.minecraft.entity.EnumCreatureType;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeDecorator;
+import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.biome.BiomeGenEnd;
-import net.minecraftforge.common.BiomeManager;
-import net.minecraftforge.common.BiomeManager.BiomeEntry;
-import net.minecraftforge.common.BiomeManager.BiomeType;
 import chylex.hee.entity.mob.EntityMobEnderman;
-import chylex.hee.proxy.ModCommonProxy;
 import chylex.hee.system.util.MathUtil;
 
 public final class BiomeGenHardcoreEnd extends BiomeGenEnd{
@@ -42,18 +39,16 @@ public final class BiomeGenHardcoreEnd extends BiomeGenEnd{
 			}
 		}
 		
-		if (!MathUtil.floatEquals(overworldEndermanMultiplier,1F) || ModCommonProxy.hardcoreEnderbacon){
-			for(BiomeType type:BiomeManager.BiomeType.values()){
-				for(BiomeEntry entry:BiomeManager.getBiomes(type)){
-					List<SpawnListEntry> spawnList = entry.biome.getSpawnableList(EnumCreatureType.monster);
-					
-					for(SpawnListEntry spawnEntry:spawnList){
-						if (spawnEntry.entityClass == EntityEnderman.class){
-							spawnEntry.entityClass = EntityMobEnderman.class;
-							spawnEntry.itemWeight = Math.round(spawnEntry.itemWeight*overworldEndermanMultiplier);
-							break;
-						}
-					}
+		for(BiomeGenBase biome:BiomeGenBase.getBiomeGenArray()){
+			if (biome == null)continue;
+			
+			List<SpawnListEntry> spawnList = biome.getSpawnableList(EnumCreatureType.monster);
+			
+			for(SpawnListEntry spawnEntry:spawnList){
+				if (spawnEntry.entityClass == EntityEnderman.class){
+					spawnEntry.entityClass = EntityMobEnderman.class;
+					if (!MathUtil.floatEquals(overworldEndermanMultiplier,1F))spawnEntry.itemWeight = Math.round(spawnEntry.itemWeight*overworldEndermanMultiplier);
+					break;
 				}
 			}
 		}
