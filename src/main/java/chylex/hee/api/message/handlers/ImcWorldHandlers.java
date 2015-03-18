ce6e5bfc884fe9b05efca5c4686202864c8e5274
chylex <info@chylex.com>
2015-03-18 15:13:23 +0100

    Added world remove command

--- src/main/java/chylex/hee/api/message/handlers/ImcWorldHandlers.java
+++ src/main/java/chylex/hee/api/message/handlers/ImcWorldHandlers.java
@@ -1,13 +1,20 @@
 package chylex.hee.api.message.handlers;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import net.minecraft.item.ItemStack;
+import org.apache.commons.lang3.ArrayUtils;
 import chylex.hee.api.message.MessageHandler;
 import chylex.hee.api.message.MessageRunner;
+import chylex.hee.api.message.element.IntValue;
+import chylex.hee.api.message.element.ItemPatternValue;
 import chylex.hee.api.message.element.StringValue;
 import chylex.hee.api.message.element.WeightedLootValue;
 import chylex.hee.api.message.utils.MessageLogger;
 import chylex.hee.api.message.utils.RunEvent;
 import chylex.hee.entity.block.EntityBlockHomelandCache;
+import chylex.hee.mechanics.essence.handler.DragonEssenceHandler;
+import chylex.hee.system.util.ItemPattern;
 import chylex.hee.world.loot.LootItemStack;
 import chylex.hee.world.loot.WeightedLootList;
 import chylex.hee.world.structure.island.biome.feature.forest.ravageddungeon.RavagedDungeonLoot;
@@ -60,10 +67,41 @@ public final class ImcWorldHandlers extends ImcHandler{
 		}
 	};
 	
+	private static final MessageHandler lootRemove = new MessageHandler(){
+		@Override
+		public void call(MessageRunner runner){
+			WeightedLootList list = lootNames.get(runner.getString("list"));
+			int limit = runner.getInt("limit");
+			
+			ItemPattern pattern = runner.<ItemPattern>getValue("search");
+			pattern.setDamageValues(ArrayUtils.EMPTY_INT_ARRAY);
+			pattern.setNBT(null);
+			
+			int size = list.size();
+			
+			for(Iterator<LootItemStack> iter = list.iterator(); iter.hasNext();){
+				if (pattern.matches(new ItemStack(iter.next().getItem()))){
+					iter.remove();
+					if (limit > 0 && --limit == 0)break;
+				}
+			}
+			
+			size = size-DragonEssenceHandler.recipes.size();
+			
+			if (size == 0)MessageLogger.logWarn("Did not find any items to remove.");
+			else MessageLogger.logOk("Removed $0 item(s).",size);
+		}
+	};
+	
 	@Override
 	public void register(){
 		register("HEE:World:LootAdd",lootAdd,RunEvent.LOADCOMPLETE)
 		.addProp("list",lootName)
 		.addProp("item",WeightedLootValue.any());
+		
+		register("HEE:World:LootRemove",lootRemove,RunEvent.LOADCOMPLETE)
+		.addProp("list",lootName)
+		.addProp("search",ItemPatternValue.any())
+		.addProp("limit",IntValue.positiveOrZero());
 	}
 }
