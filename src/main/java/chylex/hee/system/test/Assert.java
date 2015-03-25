a7cd01eed7583aa85a2ab666a6d17601e1c7f113
chylex <chylex@fo2.cz>
2015-03-25 19:06:27 +0100

    Fixed number object equality in Assert

--- src/main/java/chylex/hee/system/test/Assert.java
+++ src/main/java/chylex/hee/system/test/Assert.java
@@ -1,4 +1,5 @@
 package chylex.hee.system.test;
+import chylex.hee.system.util.MathUtil;
 import com.google.common.base.Objects;
 
 public final class Assert{
@@ -7,7 +8,14 @@ public final class Assert{
 	}
 	
 	public static void equal(Object value1, Object value2, String message){
-		if (!Objects.equal(value1,value2))throw new IllegalStateException(message.replace("$1",value1 == null ? "null" : value1.toString()).replace("$2",value2 == null ? "null" : value2.toString()));
+		if (!Objects.equal(value1,value2) && !areNumbersEqual(value1,value2))throw new IllegalStateException(message.replace("$1",value1 == null ? "null" : value1.toString()).replace("$2",value2 == null ? "null" : value2.toString()));
+	}
+	
+	private static boolean areNumbersEqual(Object value1, Object value2){
+		if (!(value1 instanceof Number && value2 instanceof Number))return false;
+		
+		if (value1 instanceof Double || value1 instanceof Float)return MathUtil.floatEquals(((Number)value1).floatValue(),((Number)value2).floatValue());
+		else return ((Number)value1).longValue() == ((Number)value2).longValue();
 	}
 	
 	private Assert(){}
