7ba322f6b29a8ee5dc43f22898453a03c7b2252a
chylex <info@chylex.com>
2015-03-30 13:01:18 +0200

    AltarItemRecipe now checks input NBT

--- src/main/java/chylex/hee/mechanics/essence/handler/dragon/AltarItemRecipe.java
+++ src/main/java/chylex/hee/mechanics/essence/handler/dragon/AltarItemRecipe.java
@@ -1,9 +1,12 @@
 package chylex.hee.mechanics.essence.handler.dragon;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import chylex.hee.entity.item.EntityItemAltar;
 
 public class AltarItemRecipe{
+	private static final NBTTagCompound emptyTag = new NBTTagCompound();
+	
 	public final ItemStack input, output;
 	public final short cost;
 	
@@ -11,10 +14,24 @@ public class AltarItemRecipe{
 		this.input = input;
 		this.output = output;
 		this.cost = (short)cost;
+		
+		if (input.stackTagCompound == null)input.stackTagCompound = new NBTTagCompound();
 	}
 	
-	public boolean isApplicable(ItemStack is){
-		return input.isItemEqual(is); // TODO check nbt, remove altar data from items
+	/**
+	 * Checks if the ItemStacks have the same item, damage and NBT (except for Altar status NBT).
+	 */
+	public boolean isApplicable(ItemStack is){ // TODO test
+		if (input.getItem() == is.getItem() && input.getItemDamage() == is.getItemDamage()){
+			NBTTagCompound nbt = is.stackTagCompound == null ? emptyTag : (NBTTagCompound)is.stackTagCompound.copy();
+			
+			nbt.removeTag("HEE_transform");
+			nbt.removeTag("HEE_enchant");
+			nbt.removeTag("HEE_repair");
+			
+			return nbt.equals(emptyTag);
+		}
+		else return false;
 	}
 	
 	public void doTransaction(EntityItem item){
