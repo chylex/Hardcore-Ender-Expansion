758157957e02a8fef78e4ef04a42f34de29ac6d8
chylex <info@chylex.com>
2015-04-08 20:32:07 +0200

    Added world methods to BlockPosM

--- src/main/java/chylex/hee/system/util/BlockPosM.java
+++ src/main/java/chylex/hee/system/util/BlockPosM.java
@@ -1,9 +1,18 @@
 package chylex.hee.system.util;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 
 public class BlockPosM{
+	public static AxisAlignedBB getBoundingBox(BlockPosM loc1, BlockPosM loc2){
+		return AxisAlignedBB.getBoundingBox(Math.min(loc1.x,loc2.x),loc1.y,Math.min(loc1.z,loc2.z),Math.max(loc1.x,loc2.x),loc2.y,Math.max(loc1.z,loc2.z));
+	}
+	
+	/* === TEMPORARY BLOCKPOS === */
+	
 	private static final BlockPosM temporary = new BlockPosM();
 	
 	public static BlockPosM tmp(int x, int y, int z){
@@ -26,11 +35,9 @@ public class BlockPosM{
 		return temporary.set(serialized);
 	}
 	
-	public static AxisAlignedBB getBoundingBox(BlockPosM loc1, BlockPosM loc2){
-		return AxisAlignedBB.getBoundingBox(Math.min(loc1.x,loc2.x),loc1.y,Math.min(loc1.z,loc2.z),Math.max(loc1.x,loc2.x),loc2.y,Math.max(loc1.z,loc2.z));
-	}
-	
 	public int x, y, z;
+	
+	/* === CONSTRUCTORS === */
 	
 	public BlockPosM(){}
 	
@@ -53,6 +60,8 @@ public class BlockPosM{
 	public BlockPosM(long serialized){
 		set(serialized);
 	}
+	
+	/* === POSITION SETTING === */
 	
 	public BlockPosM set(int x, int y, int z){
 		this.x = x;
@@ -77,6 +86,8 @@ public class BlockPosM{
 	public BlockPosM set(long serialized){
 		return set((int)(serialized>>38),(int)(serialized<<26>>52),(int)(serialized<<38>>38));
 	}
+	
+	/* === POSITION ALTERING === */
 	
 	public BlockPosM move(int x, int y, int z){
 		this.x += x;
@@ -116,6 +127,42 @@ public class BlockPosM{
 	public BlockPosM moveWest(){
 		return move(EnumFacing.WEST);
 	}
+	
+	/* === WORLD === */
+	
+	public boolean setAir(World world){
+		return world.setBlockToAir(x,y,z);
+	}
+	
+	public boolean setBlock(World world, Block block){
+		return world.setBlock(x,y,z,block);
+	}
+	
+	public boolean setBlock(World world, Block block, int metadata){
+		return world.setBlock(x,y,z,block,metadata,3);
+	}
+	
+	public boolean setBlock(World world, Block block, int metadata, int flags){
+		return world.setBlock(x,y,z,block,metadata,flags);
+	}
+	
+	public boolean isAir(World world){
+		return world.isAirBlock(x,y,z);
+	}
+	
+	public Block getBlock(World world){
+		return world.getBlock(x,y,z);
+	}
+	
+	public int getMetadata(World world){
+		return world.getBlockMetadata(x,y,z);
+	}
+	
+	public Material getMaterial(World world){
+		return world.getBlock(x,y,z).getMaterial();
+	}
+	
+	/* === SERIALIZATION AND UTILITIES === */
 	
 	public long toLong(){
 		return (x&(1L<<26)-1L)<<38|(y&(1L<<12)-1L)<<26|(z&(1L<<26)-1L);
