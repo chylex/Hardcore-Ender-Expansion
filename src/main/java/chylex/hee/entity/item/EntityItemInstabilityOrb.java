1b59b298c2c61613d9a2fdb474175643040b5841
chylex <info@chylex.com>
2015-04-14 12:53:35 +0200

    Removed Orb TNT check (TODO regs), added custom explosion

--- src/main/java/chylex/hee/entity/item/EntityItemInstabilityOrb.java
+++ src/main/java/chylex/hee/entity/item/EntityItemInstabilityOrb.java
@@ -1,17 +1,27 @@
 package chylex.hee.entity.item;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.enchantment.EnchantmentProtection;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.Vec3;
 import net.minecraft.util.WeightedRandomChestContent;
+import net.minecraft.world.ChunkPosition;
+import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ChestGenHooks;
 import chylex.hee.HardcoreEnderExpansion;
-import chylex.hee.entity.block.EntityBlockEnhancedTNTPrimed;
 import chylex.hee.entity.fx.FXType;
 import chylex.hee.mechanics.orb.OrbAcquirableItems;
 import chylex.hee.mechanics.orb.OrbSpawnableMobs;
@@ -19,12 +29,9 @@ import chylex.hee.mechanics.orb.WeightedItem;
 import chylex.hee.packets.PacketPipeline;
 import chylex.hee.packets.client.C21EffectEntity;
 import chylex.hee.system.logging.Log;
-import chylex.hee.system.util.DragonUtil;
+import chylex.hee.system.util.BlockPosM;
 
-public class EntityItemInstabilityOrb extends EntityItem{
-	private boolean isTntNearby = false;
-	private boolean canExplode = true;
-	
+public class EntityItemInstabilityOrb extends EntityItem{	
 	public EntityItemInstabilityOrb(World world){
 		super(world);
 	}
@@ -49,16 +56,7 @@ public class EntityItemInstabilityOrb extends EntityItem{
 		
 		if (!worldObj.isRemote){
 			age += 1+rand.nextInt(2+rand.nextInt(4))*rand.nextInt(3);
-			
 			if (age >= 666)detonate();
-			else if (!isTntNearby){
-				for(EntityTNTPrimed tnt:(List<EntityTNTPrimed>)worldObj.getEntitiesWithinAABB(EntityTNTPrimed.class,boundingBox.expand(5.2D,5.2D,5.2D))){
-					if (tnt.fuse < 3 && getDistance(tnt.posX,tnt.posY,tnt.posZ)/(tnt instanceof EntityBlockEnhancedTNTPrimed ? 5.2D : 4D) <= 1D){ // 4 = strength of TNT
-						isTntNearby = true;						
-						break;
-					}
-				}
-			}
 		}
 		else{
 			int chance = age/17-7;
@@ -71,8 +69,10 @@ public class EntityItemInstabilityOrb extends EntityItem{
 	}
 	
 	private void detonate(){
-		if (rand.nextInt(6) == 0 && canExplode){
-			DragonUtil.createExplosion(this,posX,posY,posZ,2.8F+rand.nextFloat()*0.8F,rand.nextInt(3) == 0);
+		if (rand.nextInt(6) == 0){
+			ExplosionOrb explosion = new ExplosionOrb(worldObj,this,posX,posY,posZ,2.8F+rand.nextFloat()*0.8F);
+			explosion.doExplosionA();
+			explosion.doExplosionB(true);
 		}
 		else if (rand.nextInt(6) == 0){
 			Class<?> cls = null;
@@ -133,22 +133,126 @@ public class EntityItemInstabilityOrb extends EntityItem{
 	@Override
 	public boolean attackEntityFrom(DamageSource source, float amount){
 		if (source.isExplosion()){
-			age -= 10-rand.nextInt(80);
-			if (isTntNearby)canExplode = false;
-			return false;
+			if (rand.nextInt(5) != 0){
+				age -= 10-rand.nextInt(80);
+				return false;
+			}
 		}
-		else return super.attackEntityFrom(source,amount);
+		
+		return super.attackEntityFrom(source,amount);
 	}
 	
-	@Override
-	public void writeEntityToNBT(NBTTagCompound nbt){
-		super.writeEntityToNBT(nbt);
-		nbt.setBoolean("canExplode",canExplode);
-	}
+	private static final class ExplosionOrb extends Explosion{
+		private final World worldObj;
+		private final int dist = 16;
+		private final Map<EntityPlayer,Vec3> hurtPlayers = new HashMap<>();
+		
+		public ExplosionOrb(World world, Entity sourceEntity, double x, double y, double z, float power){
+			super(world,sourceEntity,x,y,z,power);
+			this.worldObj = world;
+			isSmoking = true;
+			isFlaming = false;
+		}
+		
+		@Override
+		public void doExplosionA(){
+			float explosionSizeBackup = explosionSize;
+			
+			HashSet<ChunkPosition> affectedBlocks = new HashSet<>();
+			double tempX, tempY, tempZ, distX, distY, distZ, totalDist;
 
-	@Override
-	public void readEntityFromNBT(NBTTagCompound nbt){
-		super.readEntityFromNBT(nbt);
-		canExplode = nbt.getBoolean("canExplode");
+			for(int x = 0; x < dist; ++x){
+				for(int y = 0; y < dist; ++y){
+					for(int z = 0; z < dist; ++z){
+						if (x == 0 || x == dist-1 || y == 0 || y == dist-1 || z == 0 || z == dist-1){
+							distX = (x/(dist-1F)*2F-1F);
+							distY = (y/(dist-1F)*2F-1F);
+							distZ = (z/(dist-1F)*2F-1F);
+							totalDist = Math.sqrt(distX*distX+distY*distY+distZ*distZ);
+							
+							distX /= totalDist;
+							distY /= totalDist;
+							distZ /= totalDist;
+							
+							float affectedDistance = explosionSize*(0.7F+worldObj.rand.nextFloat()*0.6F);
+							tempX = explosionX;
+							tempY = explosionY;
+							tempZ = explosionZ;
+
+							for(float mp = 0.3F; affectedDistance > 0F; affectedDistance -= mp*0.75F){								
+								BlockPosM tmpPos = BlockPosM.tmp(tempX,tempY,tempZ);
+								Block block = tmpPos.getBlock(worldObj);
+
+								if (block.getMaterial() != Material.air){
+									float resistance = exploder != null ? exploder.func_145772_a(this,worldObj,tmpPos.x,tmpPos.y,tmpPos.z,block) : block.getExplosionResistance(exploder,worldObj,tmpPos.x,tmpPos.y,tmpPos.z,explosionX,explosionY,explosionZ);
+									affectedDistance -= (resistance+0.3F)*mp;
+								}
+
+								if (affectedDistance > 0F && (exploder == null || exploder.func_145774_a(this,worldObj,tmpPos.x,tmpPos.y,tmpPos.z,block,affectedDistance))){
+									affectedBlocks.add(new ChunkPosition(tmpPos.x,tmpPos.y,tmpPos.z));
+								}
+
+								tempX += distX*mp;
+								tempY += distY*mp;
+								tempZ += distZ*mp;
+							}
+						}
+					}
+				}
+			}
+
+			affectedBlockPositions.addAll(affectedBlocks);
+			explosionSize *= 2F;
+			
+			int minX = MathHelper.floor_double(explosionX-explosionSize-1D), maxX = MathHelper.floor_double(explosionX+explosionSize+1D);
+			int minY = MathHelper.floor_double(explosionY-explosionSize-1D), maxY = MathHelper.floor_double(explosionY+explosionSize+1D);
+			int minZ = MathHelper.floor_double(explosionZ-explosionSize-1D), maxZ = MathHelper.floor_double(explosionZ+explosionSize+1D);
+			
+			List<Entity> entities = worldObj.getEntitiesWithinAABBExcludingEntity(exploder,AxisAlignedBB.getBoundingBox(minX,minY,minZ,maxX,maxY,maxZ));
+			Vec3 locationVec = Vec3.createVectorHelper(explosionX,explosionY,explosionZ);
+
+			for(int a = 0; a < entities.size(); ++a){
+				Entity entity = entities.get(a);
+				double entityDist = entity.getDistance(explosionX,explosionY,explosionZ)/explosionSize;
+
+				if (entityDist <= 1D){
+					tempX = entity.posX-explosionX;
+					tempY = entity.posY+entity.getEyeHeight()-explosionY;
+					tempZ = entity.posZ-explosionZ;
+					totalDist = MathHelper.sqrt_double(tempX*tempX+tempY*tempY+tempZ*tempZ);
+
+					if (totalDist != 0D){
+						tempX /= totalDist;
+						tempY /= totalDist;
+						tempZ /= totalDist;
+						
+						double blastPower = (1D-entityDist)*worldObj.getBlockDensity(locationVec,entity.boundingBox);
+						
+						if (canDamageEntity(entity))entity.attackEntityFrom(DamageSource.setExplosionSource(this),((int)((blastPower*blastPower+blastPower)/2D*8D*explosionSize+1D)));
+						else continue;
+						
+						double knockbackMp = EnchantmentProtection.func_92092_a(entity,blastPower);
+						entity.motionX += tempX*knockbackMp;
+						entity.motionY += tempY*knockbackMp;
+						entity.motionZ += tempZ*knockbackMp;
+
+						if (entity instanceof EntityPlayer){
+							hurtPlayers.put((EntityPlayer)entity,Vec3.createVectorHelper(tempX*blastPower,tempY*blastPower,tempZ*blastPower));
+						}
+					}
+				}
+			}
+
+			explosionSize = explosionSizeBackup;
+		}
+		
+		@Override
+		public Map func_77277_b(){
+			return hurtPlayers;
+		}
+		
+		private boolean canDamageEntity(Entity entity){
+			return !(entity instanceof EntityLiving);
+		}
 	}
 }
