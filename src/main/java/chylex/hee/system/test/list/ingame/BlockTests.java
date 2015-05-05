6141d95b052a3ae3b296e46d3d1c7704107b62a7
chylex <info@chylex.com>
2015-05-05 19:27:28 +0200

    Work on block tests

--- src/main/java/chylex/hee/system/test/list/ingame/BlockTests.java
+++ src/main/java/chylex/hee/system/test/list/ingame/BlockTests.java
@@ -1,4 +1,6 @@
 package chylex.hee.system.test.list.ingame;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
@@ -7,6 +9,7 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import net.minecraftforge.common.DimensionManager;
@@ -19,6 +22,11 @@ import chylex.hee.system.test.data.MethodType;
 import chylex.hee.system.test.data.RunTime;
 import chylex.hee.system.test.data.UnitTest;
 import chylex.hee.system.util.BlockPosM;
+import chylex.hee.system.util.MathUtil;
+import chylex.hee.tileentity.TileEntityEnhancedBrewingStand;
+import chylex.hee.tileentity.TileEntityEssenceAltar;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Multimap;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
@@ -30,17 +38,21 @@ public class BlockTests{
 	private EntityPlayer player;
 	private BlockPosM pos;
 	
+	private Multimap<String,BlockPosM> storedLocs = HashMultimap.create();
+	
 	public void setup(){
 		if (world != null)return;
 		world = DimensionManager.getWorld(0);
 		player = Minecraft.getMinecraft().thePlayer;
 		pos = new BlockPosM();
+		storedLocs.clear();
 	}
 	
 	@UnitTest(type = MethodType.PREPARATION, runTime = RunTime.INGAME, trigger = testTrigger+"prep")
 	public void prepBlockChunk(){
 		setup();
-		player.setPositionAndUpdate(-1D,world.getTopSolidOrLiquidBlock(-1,-1),-1D);
+		
+		if (MathUtil.distance(player.posX,player.posZ) > 64D)player.setPositionAndUpdate(-1D,world.getTopSolidOrLiquidBlock(-1,-1),-1D);
 		
 		// clear area
 		for(pos.x = 0; pos.x < 16; pos.x++){
@@ -98,8 +110,15 @@ public class BlockTests{
 		setPos(0,9);
 		pos.moveUp().moveUp();
 		
-		for(int a = 0; a < 2; a++)setMove(BlockList.obsidian_falling);
-		for(int a = 0; a < 2; a++)setMove(Blocks.dragon_egg);
+		for(int a = 0; a < 2; a++){
+			storedLocs.put("FallingBlock",pos.copy());
+			setMove(BlockList.obsidian_falling);
+		}
+		
+		for(int a = 0; a < 2; a++){
+			storedLocs.put("FallingBlock",pos.copy());
+			setMove(Blocks.dragon_egg);
+		}
 		
 		// first floor - special condition decorative blocks
 		setFloor(0,11,15,Blocks.end_stone);
@@ -133,15 +152,25 @@ public class BlockTests{
 		setPos(2,0);
 		
 		pos.setX(3).setZ(3).setBlock(world,BlockList.essence_altar,EssenceType.DRAGON.id);
+		storedLocs.put("DragonEssenceAltar",pos.copy());
+		getTile(TileEntityEssenceAltar.class).loadFromDamage(EssenceType.DRAGON.id);
+		
+		try{
+			Field essenceLevel = TileEntityEssenceAltar.class.getDeclaredField("essenceLevel");
+			essenceLevel.setAccessible(true);
+			essenceLevel.set(pos.getTileEntity(world),512);
+		}catch(Exception e){
+			throw new RuntimeException(e);
+		}
 		
 		pos.setX(0).setZ(3).setBlock(world,Blocks.stonebrick);
-		spawnItem(0.5D,pos.y+1,3.5D,new ItemStack(Items.brewing_stand));
+		spawnItem(0.5D,pos.y+1.5D,3.5D,new ItemStack(Items.brewing_stand));
 		pos.setX(3).setZ(0).setBlock(world,Blocks.stonebrick);
-		spawnItem(3.5D,pos.y+1,0.5D,new ItemStack(Items.ender_eye));
+		spawnItem(3.5D,pos.y+1.5D,0.5D,new ItemStack(Items.ender_eye));
 		pos.setX(6).setZ(3).setBlock(world,Blocks.stonebrick);
-		spawnItem(6.5D,pos.y+1,3.5D,new ItemStack(ItemList.ghost_amulet,1,0));
+		spawnItem(6.5D,pos.y+1.5D,3.5D,new ItemStack(ItemList.ghost_amulet,1,0));
 		pos.setX(3).setZ(6).setBlock(world,Blocks.stonebrick);
-		spawnItem(0.5D,pos.y+1,3.5D,new ItemStack(Items.diamond_sword,1,50));
+		spawnItem(3.5D,pos.y+1.5D,6.5D,new ItemStack(Items.diamond_sword,1,50));
 		pos.setX(5).setZ(5).setBlock(world,Blocks.stonebrick);
 		pos.setX(1).setZ(5).setBlock(world,Blocks.stonebrick);
 		pos.setX(5).setZ(1).setBlock(world,Blocks.stonebrick);
@@ -150,20 +179,41 @@ public class BlockTests{
 		setFloor(2,9,15,Blocks.end_stone);
 		setPos(2,0);
 		
-		pos.setX(12).setZ(3).setBlock(world,BlockList.essence_altar,EssenceType.FIERY.id);
+		pos.setX(3).setZ(12).setBlock(world,BlockList.essence_altar,EssenceType.FIERY.id);
+		getTile(TileEntityEssenceAltar.class).loadFromDamage(EssenceType.FIERY.id);
 		
-		// third floor - enhanced brewing
+		// fourth floor - enhanced brewing
+		setFloor(3,0,3,Blocks.end_stone);
+		setPos(3,0);
+		
+		List<Object[]> potions = new ArrayList<>();
+		potions.add(new Object[]{ 0, Items.nether_wart, 0 });
+		potions.add(new Object[]{ 16, Items.sugar, 0 });
+		// TODO
+		
+		for(Object[] t:potions){
+			setMove(BlockList.enhanced_brewing_stand);
+			getTile(TileEntityEnhancedBrewingStand.class).setInventorySlotContents(1,new ItemStack(Items.potionitem,1,(Integer)t[0]));
+			getTile(TileEntityEnhancedBrewingStand.class).setInventorySlotContents(3,t[1] instanceof Item ? new ItemStack((Item)t[1]) : (ItemStack)t[1]);
+			if ((Integer)t[2] != 0)getTile(TileEntityEnhancedBrewingStand.class).setInventorySlotContents(4,new ItemStack(ItemList.end_powder,1,(Integer)t[2]));
+		}
 		
 		// fourth floor - energy
+		setFloor(3,5,15,Blocks.end_stone);
+	}
+	
+	@UnitTest(type = MethodType.TEST, runTime = RunTime.INGAME, trigger = testTrigger+"test")
+	public void testBlockChunk(){
+		
 	}
 	
 	private void setFloor(int floor, int startRow, int endRow, Block block){
-		pos.set(0,7+6*floor,startRow);
+		pos.set(0,9+6*floor,startRow);
 		for(int a = 0; a < (1+endRow-startRow)*16; a++)setMove(block);
 	}
 	
 	private void setPos(int floor, int z){
-		pos.set(0,8+6*floor,z);
+		pos.set(0,10+6*floor,z);
 	}
 	
 	private void setMove(Block block){
@@ -176,8 +226,13 @@ public class BlockTests{
 		if (pos.x >= 16)pos.setX(0).move(0,0,1);
 	}
 	
+	private <T> T getTile(Class<T> cls){
+		return (T)pos.getTileEntity(world);
+	}
+	
 	private void spawnItem(double x, double y, double z, ItemStack is){
 		EntityItem item = new EntityItem(world,x,y,z,is);
+		item.motionX = item.motionY = item.motionZ = 0D;
 		item.delayBeforeCanPickup = 10;
 		world.spawnEntityInWorld(item);
 	}
