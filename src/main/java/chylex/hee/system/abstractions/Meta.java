4b69de4cb3be154864a04d3a3496ccbae0cc2fae
chylex <info@chylex.com>
2015-08-01 00:44:34 +0200

    Abstract out metadata values

---
+++ src/main/java/chylex/hee/system/abstractions/Meta.java
@@ -0,0 +1,16 @@
+package chylex.hee.system.abstractions;
+
+public final class Meta{
+	public static final byte
+		stoneBrickPlain = 0,
+		stoneBrickMossy = 1,
+		stoneBrickCracked = 2,
+		stoneBrickChiseled = 3,
+		
+		silverfishPlain = 2,
+		silverfishMossy = 3,
+		silverfishCracked = 4,
+		silverfishChiseled = 5;
+	
+	private Meta(){}
+}
