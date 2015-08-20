715dc97d98f59d9a1f69c98ff8deaa9799d4b936
chylex <info@chylex.com>
2015-08-20 21:03:28 +0200

    Fix Damage triggering every tick and add utility methods

--- src/main/java/chylex/hee/system/abstractions/damage/Damage.java
+++ src/main/java/chylex/hee/system/abstractions/damage/Damage.java
@@ -2,6 +2,8 @@ package chylex.hee.system.abstractions.damage;
 import java.util.ArrayList;
 import java.util.List;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.util.DamageSource;
 import chylex.hee.system.abstractions.damage.source.DamagedBy;
 import chylex.hee.system.abstractions.damage.source.DamagedByEntity;
@@ -12,13 +14,11 @@ public final class Damage{
 		return new Damage(amount);
 	}
 	
-	public static Damage vanilla(float amount){
-		return new Damage(amount)
-			.addModifier(IDamageModifier.peacefulExclusion)
-			.addModifier(IDamageModifier.difficultyScaling)
-			.addModifier(IDamageModifier.armorProtection)
-			.addModifier(IDamageModifier.enchantmentProtection)
-			.addModifier(IDamageModifier.potionProtection);
+	public static Damage hostileMob(EntityMob cause){
+		return new Damage((float)cause.getEntityAttribute(SharedMonsterAttributes.attackDamage).getAttributeValue())
+			.addModifiers(IDamageModifier.peacefulExclusion,IDamageModifier.difficultyScaling)
+			.addModifiers(IDamageModifier.armorProtection,IDamageModifier.enchantmentProtection,IDamageModifier.potionProtection)
+			.setSource(cause);
 	}
 	
 	private float baseAmount;
@@ -34,6 +34,11 @@ public final class Damage{
 		return this;
 	}
 	
+	public Damage addModifiers(IDamageModifier...modifiers){
+		for(IDamageModifier modifier:modifiers)this.modifiers.add(modifier);
+		return this;
+	}
+	
 	public Damage setSource(Entity directSource){
 		this.source = directSource;
 		return this;
@@ -46,6 +51,8 @@ public final class Damage{
 	}
 	
 	public boolean deal(Entity target){
+		if (target.hurtResistantTime > 10)return false;
+		
 		DamageSource damageSource = createDamageSource();
 		List<IDamagePostProcessor> postProcessors = new ArrayList<>();
 
