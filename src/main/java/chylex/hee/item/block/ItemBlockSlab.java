c2d98cf318e3fc40b81bafc80529f0dae874f125
chylex <info@chylex.com>
2015-08-27 23:57:25 +0200

    Clean up slab itemblock code

--- src/main/java/chylex/hee/item/block/ItemBlockSlab.java
+++ src/main/java/chylex/hee/item/block/ItemBlockSlab.java
@@ -10,7 +10,7 @@ import chylex.hee.system.abstractions.Pos;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class ItemBlockSlab extends ItemBlock{ // TODO test
+public class ItemBlockSlab extends ItemBlock{
 	private final BlockInfo full;
 	
 	public ItemBlockSlab(Block block){
@@ -23,31 +23,23 @@ public class ItemBlockSlab extends ItemBlock{ // TODO test
 	@Override
 	public boolean onItemUse(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ){
 		if (is.stackSize == 0 || !player.canPlayerEdit(x,y,z,side,is))return false;
-		
-		boolean isTopSlab = (Pos.at(x,y,z).getMetadata(world)&8) != 0;
-		
-		return ((side == 1 && !isTopSlab || side == 0 && isTopSlab) && tryPlaceSlab(is,player,world,x,y,z,-1)) ||
-			   tryPlaceSlab(is,player,world,x,y,z,side) ||
-			   super.onItemUse(is,player,world,x,y,z,side,hitX,hitY,hitZ);
+		return tryPlaceSlab(is,player,world,x,y,z,side,true) || super.onItemUse(is,player,world,x,y,z,side,hitX,hitY,hitZ);
 	}
 
 	@Override
 	@SideOnly(Side.CLIENT)
 	public boolean func_150936_a(World world, int x, int y, int z, int side, EntityPlayer player, ItemStack is){
+		return tryPlaceSlab(is,player,world,x,y,z,side,false) || super.func_150936_a(world,x,y,z,side,player,is);
+	}
+
+	private boolean tryPlaceSlab(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side, boolean doPlace){
 		Pos pos = Pos.at(x,y,z);
 		boolean isTopSlab = (pos.getMetadata(world)&8) != 0;
 		
-		return ((side == 1 && !isTopSlab || side == 0 && isTopSlab) && pos.getBlock(world) == field_150939_a) ||
-				pos.offset(side).getBlock(world) == field_150939_a ||
-			   super.func_150936_a(world,x,y,z,side,player,is);
-	}
-
-	private boolean tryPlaceSlab(ItemStack is, EntityPlayer player, World world, int x, int y, int z, int side){
-		Pos pos = Pos.at(x,y,z);
-		if (side != -1)pos = pos.offset(side);
-
+		if (!((side == 1 && !isTopSlab) || (side == 0 && isTopSlab)))pos = pos.offset(side);
+		
 		if (pos.getBlock(world) == field_150939_a){
-			if (world.checkNoEntityCollision(full.block.getCollisionBoundingBoxFromPool(world,x,y,z)) && pos.setBlock(world,full)){
+			if (doPlace && world.checkNoEntityCollision(full.block.getCollisionBoundingBoxFromPool(world,x,y,z)) && pos.setBlock(world,full)){
 				SoundType sound = full.block.stepSound;
 				world.playSoundEffect(x+0.5D,y+0.5D,z+0.5D,sound.func_150496_b(),(sound.getVolume()+1F)*0.5F,sound.getPitch()*0.8F);
 				--is.stackSize;
