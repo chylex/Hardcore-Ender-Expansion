e9094b79fde77bea6780544f5402a96119cdd777
chylex <contact@chylex.com>
2015-09-07 01:51:41 +0200

    Add temporary code to test End Portal animations

--- src/main/java/chylex/hee/block/BlockEndPortalFrame.java
+++ src/main/java/chylex/hee/block/BlockEndPortalFrame.java
@@ -1,17 +1,21 @@
 package chylex.hee.block;
 import java.util.Random;
-import chylex.hee.system.abstractions.Meta;
-import chylex.hee.tileentity.TileEntityEndPortalFrame;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
+import chylex.hee.system.abstractions.Meta;
+import chylex.hee.system.abstractions.Pos;
+import chylex.hee.system.abstractions.facing.Facing4;
+import chylex.hee.tileentity.TileEntityEndPortalCustom;
+import chylex.hee.tileentity.TileEntityEndPortalFrame;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockEndPortalFrame extends BlockContainer{
 	@SideOnly(Side.CLIENT)
@@ -20,6 +24,30 @@ public class BlockEndPortalFrame extends BlockContainer{
 	public BlockEndPortalFrame(){
 		super(Material.rock);
 		setBlockBounds(0F,0F,0F,1F,0.8125F,1F);
+	}
+	
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ){
+		// TODO temp
+		Pos pos1 = Pos.at(x,y,z);
+		
+		for(Facing4 facing:Facing4.list){
+			if (pos1.offset(facing).checkBlock(world,Blocks.end_portal,Meta.endPortalDisabled)){
+				pos1 = pos1.offset(facing).offset(facing.rotateRight());
+				Pos pos2 = Pos.at(pos1).offset(facing,2).offset(facing.rotateLeft(),2);
+				
+				Pos.forEachBlock(pos1,pos2,pos -> {
+					if (pos.checkBlock(world,Blocks.end_portal,Meta.endPortalDisabled)){
+						pos.setMetadata(world,Meta.endPortalActive);
+						((TileEntityEndPortalCustom)pos.getTileEntity(world)).startAnimation();
+					}
+				});
+				
+				break;
+			}
+		}
+		
+		return true;
 	}
 	
 	@Override
