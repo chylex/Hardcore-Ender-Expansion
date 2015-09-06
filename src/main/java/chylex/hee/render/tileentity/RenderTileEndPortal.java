d10b504d29a5c325edaaf0892f40226749fdc6b0
chylex <contact@chylex.com>
2015-09-07 01:43:04 +0200

    Add a fade-in animation when the portal is activated

--- src/main/java/chylex/hee/render/tileentity/RenderTileEndPortal.java
+++ src/main/java/chylex/hee/render/tileentity/RenderTileEndPortal.java
@@ -1,9 +1,31 @@
 package chylex.hee.render.tileentity;
 import chylex.hee.system.abstractions.Meta;
+import chylex.hee.system.util.MathUtil;
+import chylex.hee.tileentity.TileEntityEndPortalCustom;
 
 public class RenderTileEndPortal extends RenderTilePortalBase{
+	private float progress;
+	
+	@Override
+	protected void onRender(){
+		if (!(tile instanceof TileEntityEndPortalCustom))throw new IllegalStateException("Invalid End Portal tile entity passed to the renderer: "+tile.getClass().getName());
+		
+		TileEntityEndPortalCustom te = (TileEntityEndPortalCustom)tile;
+		progress = te.animate ? te.prevColorProgress+(te.colorProgress-te.prevColorProgress)*ptt : 1F;
+	}
+	
 	@Override
 	protected int getLayers(){
-		return tile.getBlockMetadata() == Meta.endPortalActive ? 16 : 3;
+		return tile.getBlockMetadata() == Meta.endPortalActive ? 16 : 2;
+	}
+	
+	@Override
+	protected void generateColors(int layer){
+		super.generateColors(layer);
+		
+		if (progress < 1F && layer > 1){
+			// this is cubic easing out with adjustment to kick off new layers just before the old one has full color
+			colorMp *= MathUtil.clamp(-progress*(progress-2F)*14.5F-0.95F*(layer-1),0F,1F);
+		}
 	}
 }
--- src/main/java/chylex/hee/tileentity/TileEntityEndPortalCustom.java
+++ src/main/java/chylex/hee/tileentity/TileEntityEndPortalCustom.java
@@ -1,8 +1,35 @@
 package chylex.hee.tileentity;
 import net.minecraft.tileentity.TileEntityEndPortal;
 import net.minecraft.util.AxisAlignedBB;
+import chylex.hee.system.util.MathUtil;
 
 public class TileEntityEndPortalCustom extends TileEntityEndPortal{
+	public boolean animate = false;
+	public float colorProgress = 1F, prevColorProgress = 1F;
+	
+	@Override
+	public void updateEntity(){
+		if (worldObj.isRemote && animate){
+			if (MathUtil.floatEquals(prevColorProgress = colorProgress,1F)){
+				animate = false;
+				return;
+			}
+			else colorProgress = Math.min(colorProgress+0.0075F,1F);
+		}
+	}
+	
+	public void startAnimation(){
+		if (!worldObj.isRemote)return;
+		
+		animate = true;
+		colorProgress = prevColorProgress = 0F;
+	}
+	
+	@Override
+	public boolean canUpdate(){
+		return true; // just in case something optimized it
+	}
+	
 	@Override
 	public AxisAlignedBB getRenderBoundingBox(){
 		return AxisAlignedBB.getBoundingBox(xCoord,yCoord,zCoord,xCoord+1D,yCoord+1D,zCoord+1D);
