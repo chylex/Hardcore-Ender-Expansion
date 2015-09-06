b769506f2bb8714134b8b2258d415ccbf1b40a39
chylex <contact@chylex.com>
2015-09-06 03:27:41 +0200

    Increase size of End Portal room and add vines and walls

--- src/main/java/chylex/hee/world/feature/stronghold/rooms/general/StrongholdRoomEndPortal.java
+++ src/main/java/chylex/hee/world/feature/stronghold/rooms/general/StrongholdRoomEndPortal.java
@@ -1,6 +1,7 @@
 package chylex.hee.world.feature.stronghold.rooms.general;
 import java.util.Random;
 import net.minecraft.init.Blocks;
+import chylex.hee.init.BlockList;
 import chylex.hee.system.abstractions.BlockInfo;
 import chylex.hee.system.abstractions.Meta;
 import chylex.hee.system.abstractions.Pos.PosMutable;
@@ -13,16 +14,16 @@ import chylex.hee.world.structure.util.Size;
 
 public class StrongholdRoomEndPortal extends StrongholdPiece{ // TODO maybe add stairs to upper level
 	public StrongholdRoomEndPortal(){
-		super(Type.ROOM,new Size(17,13,17));
+		super(Type.ROOM,new Size(19,13,19));
 		
-		addConnection(Facing4.NORTH_NEGZ,8,0,0,fromRoom);
-		addConnection(Facing4.SOUTH_POSZ,8,0,16,fromRoom);
-		addConnection(Facing4.EAST_POSX,16,0,8,fromRoom);
-		addConnection(Facing4.WEST_NEGX,0,0,8,fromRoom);
-		addConnection(Facing4.NORTH_NEGZ,8,6,0,fromRoom);
-		addConnection(Facing4.SOUTH_POSZ,8,6,16,fromRoom);
-		addConnection(Facing4.EAST_POSX,16,6,8,fromRoom);
-		addConnection(Facing4.WEST_NEGX,0,6,8,fromRoom);
+		addConnection(Facing4.NORTH_NEGZ,maxX/2,0,0,fromRoom);
+		addConnection(Facing4.SOUTH_POSZ,maxX/2,0,maxZ,fromRoom);
+		addConnection(Facing4.EAST_POSX,maxX,0,maxZ/2,fromRoom);
+		addConnection(Facing4.WEST_NEGX,0,0,maxZ/2,fromRoom);
+		addConnection(Facing4.NORTH_NEGZ,maxX/2,6,0,fromRoom);
+		addConnection(Facing4.SOUTH_POSZ,maxX/2,6,maxZ,fromRoom);
+		addConnection(Facing4.EAST_POSX,maxX,6,maxZ/2,fromRoom);
+		addConnection(Facing4.WEST_NEGX,0,6,maxZ/2,fromRoom);
 	}
 
 	@Override
@@ -34,23 +35,29 @@ public class StrongholdRoomEndPortal extends StrongholdPiece{ // TODO maybe add 
 		placeWalls(world,rand,placeStoneBrick,x,y,z,x+maxX,y+maxY-1,z+maxZ); // outer wall layer
 		placeWalls(world,rand,placeStoneBrick,x+1,y,z+1,x+maxX-1,y+5,z+maxZ-1); // inner wall layer
 		
+		// second floor
+		placeOutline(world,rand,placeStoneBrick,x+1,y+6,z+1,x+maxX-1,y+6,z+maxZ-1,4); // floor outline
+		placeOutline(world,rand,IBlockPicker.basic(Blocks.fence),x+4,y+7,z+4,x+maxX-4,y+7,z+maxZ-4,1); // fence on the edge
+		
 		// first floor
-		placeOutline(world,rand,placeStoneBrick,x+2,y,z+2,x+maxX-2,y,z+maxZ-2,1); // floor outline
+		placeOutline(world,rand,placeStoneBrick,x+2,y,z+2,x+maxX-2,y,z+maxZ-2,2); // floor outline
 		
 		IBlockPicker placeSlabBottom = IBlockPicker.basic(Blocks.stone_slab,Meta.slabStoneBrickBottom);
 		
 		for(int cornerX = 0; cornerX < 2; cornerX++){
 			for(int cornerZ = 0; cornerZ < 2; cornerZ++){
 				// floor full block corners
-				placeBlock(world,rand,placeStoneBrick,x+3+10*cornerX,y,z+3+10*cornerZ);
-				placeBlock(world,rand,placeStoneBrick,x+4+8*cornerX,y,z+3+10*cornerZ);
-				placeBlock(world,rand,placeStoneBrick,x+3+10*cornerX,y,z+4+8*cornerZ);
+				placeBlock(world,rand,placeStoneBrick,x+4+10*cornerX,y,z+4+10*cornerZ);
+				placeBlock(world,rand,placeStoneBrick,x+5+8*cornerX,y,z+4+10*cornerZ);
+				placeBlock(world,rand,placeStoneBrick,x+4+10*cornerX,y,z+5+8*cornerZ);
 				// slabs in portal corners
-				placeBlock(world,rand,placeSlabBottom,x+6+4*cornerX,y,z+6+4*cornerZ);
+				placeBlock(world,rand,placeSlabBottom,x+7+4*cornerX,y,z+7+4*cornerZ);
+				// walls in corners
+				placeLine(world,rand,IBlockPicker.basic(BlockList.stone_brick_wall),x+3+12*cornerX,y+1,z+3+12*cornerZ,x+3+12*cornerX,y+6,z+3+12*cornerZ);
 			}
 		}
 		
-		placeOutline(world,rand,placeSlabBottom,x+5,y,z+5,x+maxX-5,y,z+maxZ-5,1); // slabs around portal
+		placeOutline(world,rand,placeSlabBottom,x+6,y,z+6,x+maxX-6,y,z+maxZ-6,1); // slabs around portal
 		
 		for(Facing4 facing:Facing4.list){
 			int perX = facing.perpendicular().getX(), perZ = facing.perpendicular().getZ();
@@ -70,11 +77,11 @@ public class StrongholdRoomEndPortal extends StrongholdPiece{ // TODO maybe add 
 			placeBlock(world,rand,placeStoneBrickStairs(facing,false),mpos.x+perX*3,y,mpos.z+perZ*3);
 			placeBlock(world,rand,placeStoneBrickStairs(facing.rotateLeft(),false),mpos.x+leftX*4,y,mpos.z+leftZ*4);
 			placeBlock(world,rand,placeStoneBrickStairs(facing.rotateRight(),false),mpos.x+rightX*4,y,mpos.z+rightZ*4);
+			
+			// vines in corners
+			mpos.set(x+maxX/2,y,z+maxZ/2).move(facing,7).move(facing = facing.rotateRight(),7);
+			placeLine(world,rand,IBlockPicker.basic(BlockList.dry_vine,Meta.getVine(facing,facing.rotateLeft())),mpos.x,y+1,mpos.z,mpos.x,y+5,mpos.z);
 		}
-		
-		// second floor
-		placeOutline(world,rand,placeStoneBrick,x+1,y+6,z+1,x+maxX-1,y+6,z+maxZ-1,3); // floor outline
-		placeOutline(world,rand,IBlockPicker.basic(Blocks.fence),x+3,y+7,z+3,x+maxX-3,y+7,z+maxZ-3,1); // fence on the edge
 		
 		// general		
 		for(Facing4 facing:Facing4.list){
@@ -84,7 +91,7 @@ public class StrongholdRoomEndPortal extends StrongholdPiece{ // TODO maybe add 
 			boolean bottomUnused = inst.isConnectionFree(facing,connection -> connection.offsetY == 0);
 			boolean topUnused = inst.isConnectionFree(facing,connection -> connection.offsetY == 6);
 			
-			mpos.set(x+maxX/2,y+1,z+maxZ/2).move(facing,8);
+			mpos.set(x+maxX/2,y+1,z+maxZ/2).move(facing,9);
 			if (!bottomUnused)placeCube(world,rand,placeAir,mpos.x-perX,y+1,mpos.z-perZ,mpos.x+perX,y+3,mpos.z+perZ); // bottom side doors
 			if (!topUnused)placeCube(world,rand,placeAir,mpos.x-perX,y+7,mpos.z-perZ,mpos.x+perX,y+9,mpos.z+perZ); // top side doors
 			
