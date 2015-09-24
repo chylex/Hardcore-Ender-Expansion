f950c6cefe6849705cb8689abe1fba0ad7634e96
chylex <info@chylex.com>
2015-09-24 18:14:38 +0200

    Readd vanilla sound effect to Eye of Ender throwing

--- src/main/java/chylex/hee/mechanics/MiscEvents.java
+++ src/main/java/chylex/hee/mechanics/MiscEvents.java
@@ -5,6 +5,7 @@ import net.minecraft.entity.monster.EntitySilverfish;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.player.EntityInteractEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -21,8 +22,13 @@ public class MiscEvents{
 	@SubscribeEvent
 	public void onItemUse(PlayerInteractEvent e){
 		if (e.action == Action.RIGHT_CLICK_AIR && !e.world.isRemote && e.entityPlayer.getHeldItem() != null && e.entityPlayer.getHeldItem().getItem() == Items.ender_eye){
+			World world = e.world;
+			world.playSoundAtEntity(e.entity,"random.bow",0.5F,0.4F/(world.rand.nextFloat()*0.4F+0.8F));
+			world.playAuxSFXAtEntity(null,1002,(int)e.entity.posX,(int)e.entity.posY,(int)e.entity.posZ,0);
+			
 			if (!e.entityPlayer.capabilities.isCreativeMode)--e.entityPlayer.getHeldItem().stackSize;
-			e.world.spawnEntityInWorld(new EntityProjectileEyeOfEnder(e.world,e.entity));
+			world.spawnEntityInWorld(new EntityProjectileEyeOfEnder(world,e.entity));
+			
 			e.setCanceled(true);
 		}
 	}
