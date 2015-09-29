8996012d7294d8c2bfab1b34c570a689a129bbb1
chylex <contact@chylex.com>
2015-09-29 18:26:36 +0200

    Remove old Facing class and deprecate old Direction class

--- src/main/java/chylex/hee/world/util/Direction.java
+++ src/main/java/chylex/hee/world/util/Direction.java
@@ -1,8 +1,12 @@
 package chylex.hee.world.util;
 
+@Deprecated
 public final class Direction{
+	@Deprecated
 	public static final int[] offsetX = new int[]{ 0, -1, 0, 1 };
+	@Deprecated
 	public static final int[] offsetZ = new int[]{ 1, 0, -1, 0 };
+	@Deprecated
 	public static final int[] rotateOpposite = new int[]{ 2, 3, 0, 1 };
 	
 	private Direction(){}
