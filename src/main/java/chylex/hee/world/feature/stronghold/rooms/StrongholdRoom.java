49a8ba6fd2ab2466127db5ba9a16346b385dff1e
chylex <info@chylex.com>
2015-10-14 21:02:24 +0200

    Move most StrongholdRoom code to another class for convenience

--- src/main/java/chylex/hee/world/feature/stronghold/rooms/StrongholdRoom.java
+++ src/main/java/chylex/hee/world/feature/stronghold/rooms/StrongholdRoom.java
@@ -1,50 +1,15 @@
 package chylex.hee.world.feature.stronghold.rooms;
-import java.util.Random;
 import javax.annotation.Nullable;
-import org.apache.commons.lang3.ArrayUtils;
-import chylex.hee.system.abstractions.Pos.PosMutable;
 import chylex.hee.system.abstractions.facing.Facing4;
-import chylex.hee.world.feature.stronghold.StrongholdPiece;
-import chylex.hee.world.structure.StructureWorld;
-import chylex.hee.world.structure.dungeon.StructureDungeonPieceInst;
+import chylex.hee.world.feature.stronghold.StrongholdPieceGeneric;
 import chylex.hee.world.util.Size;
 
-public abstract class StrongholdRoom extends StrongholdPiece{
+public abstract class StrongholdRoom extends StrongholdPieceGeneric{
 	public StrongholdRoom(Size size){
 		super(Type.ROOM,size);
-		
-		addConnection(Facing4.NORTH_NEGZ,maxX/2,0,0,fromRoom);
-		addConnection(Facing4.SOUTH_POSZ,maxX/2,0,maxZ,fromRoom);
-		addConnection(Facing4.EAST_POSX,maxX,0,maxZ/2,fromRoom);
-		addConnection(Facing4.WEST_NEGX,0,0,maxZ/2,fromRoom);
 	}
 	
 	public StrongholdRoom(Size size, @Nullable Facing4[] connectWith){
-		super(Type.ROOM,size);
-		
-		if (ArrayUtils.contains(connectWith,Facing4.NORTH_NEGZ))addConnection(Facing4.NORTH_NEGZ,maxX/2,0,0,fromRoom);
-		if (ArrayUtils.contains(connectWith,Facing4.SOUTH_POSZ))addConnection(Facing4.SOUTH_POSZ,maxX/2,0,maxZ,fromRoom);
-		if (ArrayUtils.contains(connectWith,Facing4.EAST_POSX))addConnection(Facing4.EAST_POSX,maxX,0,maxZ/2,fromRoom);
-		if (ArrayUtils.contains(connectWith,Facing4.WEST_NEGX))addConnection(Facing4.WEST_NEGX,0,0,maxZ/2,fromRoom);
-	}
-	
-	@Override
-	public void generate(StructureDungeonPieceInst inst, StructureWorld world, Random rand, int x, int y, int z){
-		// box
-		placeCube(world,rand,placeStoneBrick,x,y,z,x+maxX,y,z+maxZ);
-		placeCube(world,rand,placeStoneBrick,x,y+maxY,z,x+maxX,y+maxY,z+maxZ);
-		placeWalls(world,rand,placeStoneBrick,x,y+1,z,x+maxX,y+maxY-1,z+maxZ);
-		
-		// connections
-		PosMutable mpos = new PosMutable();
-		
-		for(Connection connection:connections){
-			if (!inst.isConnectionFree(connection)){
-				int perX = connection.facing.perpendicular().getX(), perZ = connection.facing.perpendicular().getZ();
-				
-				mpos.set(x+connection.offsetX,y+connection.offsetY,z+connection.offsetZ);
-				placeCube(world,rand,placeAir,mpos.x-perX,mpos.y+1,mpos.z-perZ,mpos.x+perX,mpos.y+3,mpos.z+perZ);
-			}
-		}
+		super(Type.ROOM,size,connectWith);
 	}
 }
