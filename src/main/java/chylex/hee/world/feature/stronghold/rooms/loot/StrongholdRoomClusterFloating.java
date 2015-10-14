145ef274d27997d54f05b356bcd016a0b577f590
chylex <info@chylex.com>
2015-10-14 02:02:10 +0200

    Add two Cluster rooms to Stronghold

---
+++ src/main/java/chylex/hee/world/feature/stronghold/rooms/loot/StrongholdRoomClusterFloating.java
@@ -0,0 +1,43 @@
+package chylex.hee.world.feature.stronghold.rooms.loot;
+import java.util.Random;
+import net.minecraft.init.Blocks;
+import chylex.hee.init.BlockList;
+import chylex.hee.mechanics.energy.EnergyClusterGenerator;
+import chylex.hee.system.abstractions.Meta;
+import chylex.hee.tileentity.TileEntityEnergyCluster;
+import chylex.hee.world.feature.stronghold.rooms.StrongholdRoom;
+import chylex.hee.world.structure.StructureWorld;
+import chylex.hee.world.structure.dungeon.StructureDungeonPieceInst;
+import chylex.hee.world.structure.util.IBlockPicker;
+import chylex.hee.world.util.Size;
+
+public class StrongholdRoomClusterFloating extends StrongholdRoom{
+	public StrongholdRoomClusterFloating(){
+		super(new Size(9,6,9));
+	}
+	
+	@Override
+	public void generate(StructureDungeonPieceInst inst, StructureWorld world, Random rand, int x, int y, int z){
+		super.generate(inst,world,rand,x,y,z);
+		
+		final int centerX = x+maxX/2, centerZ = z+maxZ/2;
+		
+		// basic layout
+		placeOutline(world,rand,IBlockPicker.basic(Blocks.double_stone_slab,Meta.slabStoneSmoothDouble),centerX-2,y,centerZ-2,centerX+2,y,centerZ+2,1);
+		placeStairOutline(world,rand,Blocks.stone_brick_stairs,centerX,y+1,centerZ,1,true,false);
+		
+		placeStairOutline(world,rand,Blocks.stone_brick_stairs,centerX,y+maxY,centerZ,3,false,true);
+		placeOutline(world,rand,IBlockPicker.basic(Blocks.stone_slab,Meta.slabStoneSmoothTop),centerX-2,y+maxY,centerZ-2,centerX+2,y+maxY,centerZ+2,1);
+		placeStairOutline(world,rand,Blocks.stone_brick_stairs,centerX,y+maxY,centerZ,1,true,true);
+		
+		// floating cluster
+		placeBlock(world,rand,IBlockPicker.basic(Blocks.stone_slab,Meta.slabStoneBrickBottom),centerX,y+2,centerZ);
+		placeBlock(world,rand,IBlockPicker.basic(Blocks.stone_slab,Meta.slabStoneBrickTop),centerX,y+maxY-1,centerZ);
+		
+		placeBlock(world,rand,IBlockPicker.basic(BlockList.energy_cluster),centerX,y+3,centerZ);
+		
+		world.setTileEntity(centerX,y+3,centerZ,(tile, random) -> {
+			((TileEntityEnergyCluster)tile).generate(EnergyClusterGenerator.stronghold,random);
+		});
+	}
+}
---
+++ src/main/java/chylex/hee/world/feature/stronghold/rooms/loot/StrongholdRoomClusterPillar.java
@@ -0,0 +1,53 @@
+package chylex.hee.world.feature.stronghold.rooms.loot;
+import java.util.Random;
+import net.minecraft.init.Blocks;
+import chylex.hee.init.BlockList;
+import chylex.hee.mechanics.energy.EnergyClusterGenerator;
+import chylex.hee.system.abstractions.Meta;
+import chylex.hee.system.abstractions.Pos.PosMutable;
+import chylex.hee.system.abstractions.facing.Facing4;
+import chylex.hee.tileentity.TileEntityEnergyCluster;
+import chylex.hee.world.feature.stronghold.rooms.StrongholdRoom;
+import chylex.hee.world.structure.StructureWorld;
+import chylex.hee.world.structure.dungeon.StructureDungeonPieceInst;
+import chylex.hee.world.structure.util.IBlockPicker;
+import chylex.hee.world.util.Size;
+
+public class StrongholdRoomClusterPillar extends StrongholdRoom{
+	public StrongholdRoomClusterPillar(){
+		super(new Size(7,6,7));
+	}
+	
+	@Override
+	public void generate(StructureDungeonPieceInst inst, StructureWorld world, Random rand, int x, int y, int z){
+		super.generate(inst,world,rand,x,y,z);
+		
+		final int centerX = x+maxX/2, centerZ = z+maxZ/2;
+		
+		// floor and ceiling patterns
+		placeBlock(world,rand,IBlockPicker.basic(Blocks.stonebrick,Meta.stoneBrickChiseled),centerX,y,centerZ);
+		placeBlock(world,rand,IBlockPicker.basic(Blocks.stonebrick,Meta.stoneBrickChiseled),centerX,y+maxY,centerZ);
+		
+		placeStairOutline(world,rand,Blocks.stone_brick_stairs,centerX,y+maxY,centerZ,2,false,true);
+		placeStairOutline(world,rand,Blocks.stone_brick_stairs,centerX,y+maxY,centerZ,1,true,true);
+		
+		PosMutable mpos = new PosMutable();
+		
+		for(Facing4 facing:Facing4.list){
+			mpos.set(centerX,0,centerZ).move(facing,2).move(facing.rotateRight());
+			placeBlock(world,rand,IBlockPicker.basic(Blocks.stone_brick_stairs,Meta.getStairs(facing.rotateLeft(),false)),mpos.x,y,mpos.z);
+			mpos.move(facing.opposite());
+			placeBlock(world,rand,IBlockPicker.basic(Blocks.stone_brick_stairs,Meta.getStairs(facing.rotateLeft(),false)),mpos.x,y,mpos.z);
+			mpos.move(facing.rotateRight());
+			placeBlock(world,rand,IBlockPicker.basic(Blocks.stone_brick_stairs,Meta.getStairs(facing.opposite(),false)),mpos.x,y,mpos.z);
+		}
+		
+		// pillar and cluster
+		placeLine(world,rand,IBlockPicker.basic(BlockList.stone_brick_wall),centerX,y+1,centerZ,centerX,y+maxY-1,centerZ);
+		placeBlock(world,rand,IBlockPicker.basic(BlockList.energy_cluster),centerX,y+2,centerZ);
+		
+		world.setTileEntity(centerX,y+2,centerZ,(tile, random) -> {
+			((TileEntityEnergyCluster)tile).generate(EnergyClusterGenerator.stronghold,random);
+		});
+	}
+}
