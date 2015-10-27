dfc6a89271181a9129becadc07f39cd27596d4ba
chylex <info@chylex.com>
2015-10-27 23:18:54 +0100

    Add KeyState class for tracking keys outside of keybindings

---
+++ src/main/java/chylex/hee/gui/helpers/KeyState.java
@@ -0,0 +1,26 @@
+package chylex.hee.gui.helpers;
+import gnu.trove.map.hash.TIntObjectHashMap;
+import org.apache.commons.lang3.BooleanUtils;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@SideOnly(Side.CLIENT)
+public final class KeyState{
+	private static final TIntObjectHashMap<Boolean> keyMap = new TIntObjectHashMap<>(8);
+	
+	public static void startTracking(int keyCode){
+		keyMap.putIfAbsent(keyCode,Boolean.FALSE);
+	}
+	
+	public static void stopTracking(int keyCode){
+		keyMap.remove(keyCode);
+	}
+	
+	public static void setState(int keyCode, boolean isHeld){
+		if (keyMap.contains(keyCode))keyMap.put(keyCode,Boolean.valueOf(isHeld));
+	}
+	
+	public static boolean isHeld(int keyCode){
+		return BooleanUtils.isTrue(keyMap.get(keyCode));
+	}
+}
