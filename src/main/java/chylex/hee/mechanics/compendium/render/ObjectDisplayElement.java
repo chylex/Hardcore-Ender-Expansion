1be7573398bea5ec4c4748bfdf487f5d7a11dd63
chylex <info@chylex.com>
2015-10-30 18:24:25 +0100

    Work on Knowledge Object rendering

--- src/main/java/chylex/hee/mechanics/compendium/render/ObjectDisplayElement.java
+++ src/main/java/chylex/hee/mechanics/compendium/render/ObjectDisplayElement.java
@@ -1,4 +1,7 @@
 package chylex.hee.mechanics.compendium.render;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
@@ -7,18 +10,32 @@ import org.lwjgl.opengl.GL11;
 import chylex.hee.game.save.types.player.CompendiumFile;
 import chylex.hee.gui.GuiEnderCompendium;
 import chylex.hee.gui.helpers.GuiHelper;
+import chylex.hee.mechanics.compendium.content.KnowledgeFragment;
 import chylex.hee.mechanics.compendium.content.KnowledgeObject;
+import chylex.hee.mechanics.compendium.content.fragments.KnowledgeFragmentType;
 import chylex.hee.mechanics.compendium.content.objects.IObjectHolder;
 
 public class ObjectDisplayElement{
-	private enum BackgroundTile{
-		PLAIN(113,0), DISABLED(113,23), CHECKERED(136,0), BRIGHT(136,23), GOLD(159,0);
+	private enum ObjectShape{
+		PLAIN(0,0,0,0), SPECIAL(0,0,0,0);
 		
-		final byte x, y;
+		final int x, y, w, h;
 		
-		BackgroundTile(int x, int y){
-			this.x = (byte)x;
-			this.y = (byte)y;
+		private ObjectShape(int x, int y, int w, int h){
+			this.x = x;
+			this.y = y;
+			this.w = w;
+			this.h = h;
+		}
+	}
+	
+	private enum ObjectOutline{
+		NONE_UNLOCKED(0), ALL_UNLOCKED(0), ALL_BUT_SECRET(0), UNREAD_HINT(0), ESSENTIAL_ONLY(0), DEFAULT(0);
+		
+		final int color;
+		
+		private ObjectOutline(int color){
+			this.color = color;
 		}
 	}
 	
@@ -33,7 +50,7 @@ public class ObjectDisplayElement{
 	public void renderLine(GuiScreen gui, CompendiumFile file, int yLowerBound, int yUpperBound){
 		if (file.getDiscoveryDistance(object) == CompendiumFile.distanceLimit)return;
 		
-		final int offX = gui.width/2, offY = this.y; // TODO handle bounds
+		final int offX = gui.width/2, offY = this.y;
 		int brightness = 224;
 		
 		for(KnowledgeObject<?> child:object.getChildren()){
@@ -43,7 +60,9 @@ public class ObjectDisplayElement{
 		
 		final int color = (255<<24)|(brightness<<16)|(brightness<<8)|brightness;
 		
-		object.connectToChildren((x1, y1, x2, y2) -> GuiHelper.renderLine(offX+x1,offY+y1,offX+x2,offY+y2,color));
+		object.connectToChildren((x1, y1, x2, y2) -> {
+			if (!(offY+y1 > yUpperBound || offY+y2 < yLowerBound))GuiHelper.renderLine(offX+x1,offY+y1,offX+x2,offY+y2,color); // TODO test line rendering
+		});
 	}
 	
 	public void renderObject(GuiScreen gui, CompendiumFile file, int yLowerBound, int yUpperBound){
@@ -55,8 +74,6 @@ public class ObjectDisplayElement{
 		GL11.glEnable(GL11.GL_BLEND);
 		GL11.glBlendFunc(GL11.GL_SRC_ALPHA,GL11.GL_ONE_MINUS_SRC_ALPHA);
 		GL11.glColor4f(1F,1F,1F,1F);
-		
-		BackgroundTile tile = file.isDiscovered(object) ? BackgroundTile.PLAIN : BackgroundTile.DISABLED;
 		
 		/* TODO if (compendiumFile.hasDiscoveredObject(object)){
 			boolean hasAll = true;
@@ -82,9 +99,28 @@ public class ObjectDisplayElement{
 	public static void renderObject(KnowledgeObject<?> object, int x, int y, CompendiumFile file, Gui gui){
 		Minecraft mc = Minecraft.getMinecraft();
 		
+		// shape
+		
+		// outline
+		ObjectOutline outline = ObjectOutline.DEFAULT;
+		
+		Set<Entry<KnowledgeFragment,Boolean>> fragments = object.getFragments().stream().collect(Collectors.toMap(f -> f,f -> file.canSeeFragment(object,f))).entrySet();
+		Set<KnowledgeFragment> unlocked = fragments.stream().filter(entry -> entry.getValue().booleanValue()).map(entry -> entry.getKey()).collect(Collectors.toSet());
+		
+		if (unlocked.isEmpty())outline = ObjectOutline.NONE_UNLOCKED;
+		else{
+			if (unlocked.size() == fragments.size())outline = ObjectOutline.ALL_UNLOCKED;
+			else if (unlocked.stream().allMatch(fragment -> fragment.getType() == KnowledgeFragmentType.ESSENTIAL))outline = ObjectOutline.ESSENTIAL_ONLY;
+			else if (unlocked.stream().allMatch(fragment -> fragment.getType() != KnowledgeFragmentType.SECRET))outline = ObjectOutline.ALL_BUT_SECRET;
+			// TODO
+		}
+		
+		// render background
 		RenderHelper.disableStandardItemLighting();
 		mc.getTextureManager().bindTexture(GuiEnderCompendium.texBack);
 		// TODO gui.drawTexturedModalRect(x-11,y-11,tile.x,tile.y,22,22);
+		
+		// render item
 		RenderHelper.enableGUIStandardItemLighting();
 		GuiEnderCompendium.renderItem.renderItemIntoGUI(mc.fontRenderer,mc.getTextureManager(),object.holder.getDisplayItemStack(),x+3,y+3,true);
 	}
