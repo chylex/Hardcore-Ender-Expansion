10b919a65518031e179de4b922ab7bbaeefe6a73
chylex <info@chylex.com>
2015-11-03 23:39:08 +0100

    Add AIToggle and new method to AIUtil

---
+++ src/main/java/chylex/hee/entity/mob/ai/AIToggle.java
@@ -0,0 +1,31 @@
+package chylex.hee.entity.mob.ai;
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityAITasks;
+
+public final class AIToggle<T extends EntityAIBase>{
+	private final EntityAITasks taskList;
+	private final int priority;
+	private final T task;
+	private boolean enabled;
+	
+	public AIToggle(EntityAITasks taskList, int priority, T task){
+		this.taskList = taskList;
+		this.priority = priority;
+		this.task = task;
+	}
+	
+	public void changeState(boolean enableTask){
+		if (enableTask && !enabled)taskList.addTask(priority,task);
+		else if (!enableTask && enabled)taskList.removeTask(task);
+		
+		enabled = enableTask;
+	}
+	
+	public T getTask(){
+		return task;
+	}
+	
+	public boolean isEnabled(){
+		return enabled;
+	}
+}
--- src/main/java/chylex/hee/entity/mob/ai/AIUtil.java
+++ src/main/java/chylex/hee/entity/mob/ai/AIUtil.java
@@ -1,9 +1,15 @@
 package chylex.hee.entity.mob.ai;
 import java.util.ArrayList;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.ai.EntityAITasks.EntityAITaskEntry;
 
 public final class AIUtil{
+	public static void clearEntityTasks(EntityLiving entity){
+		clearTasks(entity.tasks);
+		clearTasks(entity.targetTasks);
+	}
+	
 	public static void clearTasks(EntityAITasks taskList){
 		for(EntityAITaskEntry entry:new ArrayList<EntityAITaskEntry>(taskList.taskEntries)){
 			taskList.removeTask(entry.action);
