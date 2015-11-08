99111975cccd3c8d9650a259eefe708deed9cf5f
chylex <info@chylex.com>
2015-11-08 16:52:17 +0100

    Update MoveTo particle behavior to use Vec and tweak check distance

--- src/main/java/chylex/hee/entity/fx/behavior/ParticleBehaviorMoveTo.java
+++ src/main/java/chylex/hee/entity/fx/behavior/ParticleBehaviorMoveTo.java
@@ -1,19 +1,16 @@
 package chylex.hee.entity.fx.behavior;
 import net.minecraft.client.particle.EntityFX;
-import net.minecraft.util.Vec3;
+import chylex.hee.system.abstractions.Vec;
 import chylex.hee.system.util.MathUtil;
 
 public class ParticleBehaviorMoveTo implements IParticleBehavior{
-	private final Vec3 motionVec;
+	private final Vec motionVec;
 	private final double targetX,targetY,targetZ;
 	private final double checkDist;
 	
 	public ParticleBehaviorMoveTo(EntityFX particle, double targetX, double targetY, double targetZ, float speedMultiplier){
-		motionVec = Vec3.createVectorHelper((this.targetX = targetX)-particle.posX,(this.targetY = targetY)-particle.posY,(this.targetZ = targetZ)-particle.posZ).normalize();
-		motionVec.xCoord *= speedMultiplier;
-		motionVec.yCoord *= speedMultiplier;
-		motionVec.zCoord *= speedMultiplier;
-		checkDist = speedMultiplier*0.6D;
+		motionVec = Vec.xyz((this.targetX = targetX)-particle.posX,(this.targetY = targetY)-particle.posY,(this.targetZ = targetZ)-particle.posZ).normalized().multiplied(speedMultiplier);
+		checkDist = speedMultiplier*0.51D;
 	}
 	
 	@Override
@@ -22,7 +19,7 @@ public class ParticleBehaviorMoveTo implements IParticleBehavior{
 		particle.prevPosY = particle.posY;
 		particle.prevPosZ = particle.posZ;
 		
-		particle.moveEntity(motionVec.xCoord,motionVec.yCoord,motionVec.zCoord);
+		particle.moveEntity(motionVec.x,motionVec.y,motionVec.z);
 		
 		if (MathUtil.distance(targetX-particle.posX,targetY-particle.posY,targetZ-particle.posZ) < checkDist)particle.setDead();
 	}
