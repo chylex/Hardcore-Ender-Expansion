db703d7158a9c93aab2eba3c8ff0803b1814a191
chylex <info@chylex.com>
2015-11-10 19:53:38 +0100

    Add End Powder Ore generation to The Hub

--- src/main/java/chylex/hee/world/end/gen/TerritoryTheHub.java
+++ src/main/java/chylex/hee/world/end/gen/TerritoryTheHub.java
@@ -1,14 +1,20 @@
 package chylex.hee.world.end.gen;
 import java.util.Random;
 import net.minecraft.init.Blocks;
+import chylex.hee.init.BlockList;
 import chylex.hee.system.abstractions.BlockInfo;
 import chylex.hee.system.abstractions.Meta;
 import chylex.hee.world.end.TerritoryGenerator;
 import chylex.hee.world.feature.noise.GenerateIslandNoise;
+import chylex.hee.world.feature.ores.GenerateOres;
+import chylex.hee.world.feature.ores.IOreGenerator;
 import chylex.hee.world.structure.StructureWorld;
+import chylex.hee.world.util.RandomAmount;
 
 public class TerritoryTheHub extends TerritoryGenerator{
 	private final GenerateIslandNoise island;
+	private final GenerateOres endPowderOreMain;
+	private final GenerateOres endPowderOreSurface;
 	
 	public TerritoryTheHub(StructureWorld world, Random rand){
 		super(world,rand);
@@ -18,6 +24,22 @@ public class TerritoryTheHub extends TerritoryGenerator{
 		this.island.noiseHeight = 20;
 		this.island.peakSmoothness = 50D;
 		this.island.surfaceHillScale = 92F;
+		
+		this.endPowderOreMain = new GenerateOres(Blocks.end_stone,BlockList.end_powder_ore);
+		this.endPowderOreMain.setY(10,50);
+		this.endPowderOreMain.setChunkSize(24);
+		this.endPowderOreMain.setAttemptsPerChunk(36);
+		this.endPowderOreMain.setClustersPerChunk(8,14);
+		this.endPowderOreMain.setOresPerCluster(2,6,RandomAmount.preferSmaller);
+		this.endPowderOreMain.setOreGenerator(new IOreGenerator.AdjacentSpread(true));
+		
+		this.endPowderOreSurface = new GenerateOres(Blocks.end_stone,BlockList.end_powder_ore);
+		this.endPowderOreSurface.setY(36,50);
+		this.endPowderOreSurface.setChunkSize(24);
+		this.endPowderOreSurface.setAttemptsPerChunk(18);
+		this.endPowderOreSurface.setClustersPerChunk(6,9);
+		this.endPowderOreSurface.setOresPerCluster(3,9,RandomAmount.preferSmaller);
+		this.endPowderOreSurface.setOreGenerator(new IOreGenerator.AdjacentSpread(true));
 	}
 	
 	@Override
@@ -41,5 +63,8 @@ public class TerritoryTheHub extends TerritoryGenerator{
 				}
 			}
 		}
+		
+		endPowderOreMain.generateSplit(world,rand);
+		endPowderOreSurface.generateSplit(world,rand);
 	}
 }
