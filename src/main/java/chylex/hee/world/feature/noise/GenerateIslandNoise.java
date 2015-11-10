f4b11e79151bf4393038a8625dd65279be47dc78
chylex <info@chylex.com>
2015-11-10 15:31:33 +0100

    Figure out noise generation (make GenerateIslandNoise modular and deobfuscate it)

--- src/main/java/chylex/hee/world/feature/noise/GenerateIslandNoise.java
+++ src/main/java/chylex/hee/world/feature/noise/GenerateIslandNoise.java
@@ -13,6 +13,56 @@ public class GenerateIslandNoise{
 	private double[] noiseData1, noiseData2, noiseData3;
 	private double[] densities;
 	
+	/**
+	 * Sets the maximum size of the island on X and Z axis.
+	 */
+	public int terrainSize = 100;
+	
+	/**
+	 * Sets the noise height multiplier. Default value is 32, which has the potential to fill up 128 blocks in height, but due to smoothing it generally only goes up to ~82.
+	 */
+	public int noiseHeight = 32;
+	
+	/**
+	 * Sets how steep or smooth peaks are. Default value is 80, lower values increase height variety and chaos, higher values make the terrain smoother.
+	 */
+	public double peakSmoothness = 80D;
+	
+	/**
+	 * Sets how much the island stretches the sides. Default value is 160, lower values create sharper and longer edges, higher values make them smoother.
+	 */
+	public double sideSmoothness = 160D;
+	
+	/**
+	 * Modifies the shape of density generators. Values above 1 create extreme peaks and valleys, values below 1 make the island shape more circular.
+	 */
+	public double densityPeakMultiplier = 1D;
+	
+	/**
+	 * Modifies appearance of peaks. No idea how to describe what it does. Best used with large {@code densityPeakMultiplier}.
+	 */
+	public double densityVariantLow = 1D;
+	
+	/**
+	 * Modifies appearance of peaks. No idea how to describe what it does. Best used with large {@code densityPeakMultiplier}.
+	 */
+	public double densityVariantHigh = 1D;
+	
+	/**
+	 * Sets where the surface part of the island begins.
+	 */
+	public float surfaceStart = 0.45F;
+	
+	/**
+	 * Sets scale for hill height on the island surface. Default value is 64, values between 0 and 1 create completely flat surface. Higher values might exceed the height limit.
+	 */
+	public float surfaceHillScale = 64F;
+	
+	/**
+	 * Sets smoothing value for surface hills. Default value is 3000, higher values create flatter surface, lower values increase height and variety, but might exceed the height limit.
+	 */
+	public double surfaceHeightSmoothing = 3000D;
+	
 	public GenerateIslandNoise(Block block, Random rand){
 		this.block = block;
 		this.noiseGen1 = new NoiseGeneratorOctaves(rand,16);
@@ -25,40 +75,33 @@ public class GenerateIslandNoise{
 		
 		double noiseScaleXZ = 1368.824D, noiseScaleY = 684.412D;
 		
-		noiseData1 = noiseGen3.generateNoiseOctaves(noiseData1,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ/80D,noiseScaleY/160D,noiseScaleXZ/80D);
-		noiseData2 = noiseGen1.generateNoiseOctaves(noiseData2,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ,noiseScaleY,noiseScaleXZ);
-		noiseData3 = noiseGen2.generateNoiseOctaves(noiseData3,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ,noiseScaleY,noiseScaleXZ);
+		noiseData1 = noiseGen3.generateNoiseOctaves(noiseData1,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ/peakSmoothness,noiseScaleY/sideSmoothness,noiseScaleXZ/peakSmoothness);
+		noiseData2 = noiseGen1.generateNoiseOctaves(noiseData2,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ*densityPeakMultiplier,noiseScaleY*densityVariantLow,noiseScaleXZ*densityPeakMultiplier);
+		noiseData3 = noiseGen2.generateNoiseOctaves(noiseData3,x,y,z,sizeX,sizeY,sizeZ,noiseScaleXZ*densityPeakMultiplier,noiseScaleY*densityVariantHigh,noiseScaleXZ*densityPeakMultiplier);
 		
 		int indexFull = 0;
-		int topPart = sizeY/2-2;
+		
+		int topPart = (int)(sizeY*surfaceStart);
+		int bottomPart = 8;
 
 		for(int xx = 0; xx < sizeX; xx++){
 			for(int zz = 0; zz < sizeZ; zz++){
 				float distanceX = xx+x;
 				float distanceZ = zz+z;
 				
-				float f2 = 100F-MathHelper.sqrt_float(distanceX*distanceX+distanceZ*distanceZ)*8F;
-				f2 = MathUtil.clamp(f2,-100F,80F);
-
+				float densityOffset = MathUtil.clamp(terrainSize-MathHelper.sqrt_float(distanceX*distanceX+distanceZ*distanceZ)*8F,-100F,80F)-8F;
+				
 				for(int yy = 0; yy < sizeY; ++yy){
-					double density = 0D;
-					
 					double lowestDensity = noiseData2[indexFull]/512D;
 					double highestDensity = noiseData3[indexFull]/512D;
 					double densityInterpolation = MathUtil.clamp((noiseData1[indexFull]/10D+1D)/2D,0D,1D);
-					density = lowestDensity+(highestDensity-lowestDensity)*densityInterpolation;
-					
-					density -= 8D;
-					density += f2;
+					double density = lowestDensity+(highestDensity-lowestDensity)*densityInterpolation+densityOffset;
 					
 					if (yy > topPart){
-						double d10 = MathUtil.clamp((yy-topPart)/64F,0D,1D);
-						density = density*(1D-d10)-3000D*d10;
+						double smoothSurface = MathUtil.clamp((yy-topPart)/surfaceHillScale,0D,1D);
+						density = density*(1D-smoothSurface)-surfaceHeightSmoothing*smoothSurface;
 					}
-					
-					byte bottomPart = 8;
-
-					if (yy < bottomPart){
+					else if (yy < bottomPart){
 						double smoothBottom = (bottomPart-yy)/(bottomPart-1F);
 						density = density*(1D-smoothBottom)-30D*smoothBottom;
 					}
@@ -74,48 +117,48 @@ public class GenerateIslandNoise{
 	
 	private void generateChunk(StructureWorld world, int x, int z){
 		int noiseSizeXZ = 3;
-		byte height = 33;
+		byte height = (byte)(noiseHeight+1);
 		densities = initializeNoiseField(densities,x*2,0,z*2,noiseSizeXZ,height,noiseSizeXZ);
 		
 		for(int xx = 0; xx < 2; xx++){
 			for(int zz = 0; zz < 2; zz++){
-				for(int yy = 0; yy < 32; yy++){
+				for(int yy = 0; yy < noiseHeight; yy++){
 					double verticalSmoothing = 0.25D;
 					double dBL = densities[((xx+0)*noiseSizeXZ+zz+0)*height+yy];
 					double dBR = densities[((xx+0)*noiseSizeXZ+zz+1)*height+yy];
 					double dTL = densities[((xx+1)*noiseSizeXZ+zz+0)*height+yy];
 					double dTR = densities[((xx+1)*noiseSizeXZ+zz+1)*height+yy];
-					double dTopBL = (densities[((xx+0)*noiseSizeXZ+zz+0)*height+yy+1]-dBL)*verticalSmoothing;
-					double dTopBR = (densities[((xx+0)*noiseSizeXZ+zz+1)*height+yy+1]-dBR)*verticalSmoothing;
-					double dTopTL = (densities[((xx+1)*noiseSizeXZ+zz+0)*height+yy+1]-dTL)*verticalSmoothing;
-					double dTopTR = (densities[((xx+1)*noiseSizeXZ+zz+1)*height+yy+1]-dTR)*verticalSmoothing;
+					double dOffsetBL = (densities[((xx+0)*noiseSizeXZ+zz+0)*height+yy+1]-dBL)*verticalSmoothing;
+					double dOffsetBR = (densities[((xx+0)*noiseSizeXZ+zz+1)*height+yy+1]-dBR)*verticalSmoothing;
+					double dOffsetTL = (densities[((xx+1)*noiseSizeXZ+zz+0)*height+yy+1]-dTL)*verticalSmoothing;
+					double dOffsetTR = (densities[((xx+1)*noiseSizeXZ+zz+1)*height+yy+1]-dTR)*verticalSmoothing;
 					
 					for(int yChunk = 0; yChunk < 4; yChunk++){
 						double horizontalSmoothing = 0.125D;
-						double d10 = dBL;
-						double d11 = dBR;
-						double d12 = (dTL-dBL)*horizontalSmoothing;
-						double d13 = (dTR-dBR)*horizontalSmoothing;
+						double dCurrentLeft = dBL;
+						double dCurrentRight = dBR;
+						double dOffsetLeft = (dTL-dBL)*horizontalSmoothing;
+						double dOffsetRight = (dTR-dBR)*horizontalSmoothing;
 						
 						for(int yBlock = 0; yBlock < 8; yBlock++){
-							int index = yBlock+xx*8<<11|zz*8<<7|yy*4+yChunk;
-							double d15 = d10;
-							double d16 = (d11-d10)*0.125D;
+							int index = yBlock+xx*8<<12|zz*8<<8|yy*4+yChunk;
+							double density = dCurrentLeft;
+							double interpolation = (dCurrentRight-dCurrentLeft)*0.125D;
 							
 							for(int yBlock3 = 0; yBlock3 < 8; yBlock3++){
-								if (d15 > 0D)world.setBlock(x*16+((index>>11)&15),index&127,z*16+((index>>7)&15),block);
-								index += 128;
-								d15 += d16;
+								if (density > 0D)world.setBlock(x*16+((index>>12)&15),index&255,z*16+((index>>8)&15),block);
+								index += 256;
+								density += interpolation;
 							}
 							
-							d10 += d12;
-							d11 += d13;
+							dCurrentLeft += dOffsetLeft;
+							dCurrentRight += dOffsetRight;
 						}
 						
-						dBL += dTopBL;
-						dBR += dTopBR;
-						dTL += dTopTL;
-						dTR += dTopTR;
+						dBL += dOffsetBL;
+						dBR += dOffsetBR;
+						dTL += dOffsetTL;
+						dTR += dOffsetTR;
 					}
 				}
 			}
