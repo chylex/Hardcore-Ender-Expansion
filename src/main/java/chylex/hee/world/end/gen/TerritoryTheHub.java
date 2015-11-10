35048efe659c495a392f521cf086f0ea4eac4748
chylex <info@chylex.com>
2015-11-10 01:28:00 +0100

    Add a basic island and Portal generator to The Hub

--- src/main/java/chylex/hee/world/end/gen/TerritoryTheHub.java
+++ src/main/java/chylex/hee/world/end/gen/TerritoryTheHub.java
@@ -1,17 +1,35 @@
 package chylex.hee.world.end.gen;
 import java.util.Random;
 import net.minecraft.init.Blocks;
-import chylex.hee.system.abstractions.Pos;
+import chylex.hee.system.abstractions.Meta;
 import chylex.hee.world.end.TerritoryGenerator;
+import chylex.hee.world.feature.noise.GenerateIslandNoise;
 import chylex.hee.world.structure.StructureWorld;
 
 public class TerritoryTheHub extends TerritoryGenerator{
+	private final GenerateIslandNoise island;
+	
 	public TerritoryTheHub(StructureWorld world, Random rand){
 		super(world,rand);
+		this.island = new GenerateIslandNoise(Blocks.end_stone,rand);
 	}
 	
 	@Override
 	public void generate(){
-		Pos.forEachBlock(Pos.at(-size,3,-size),Pos.at(size,3,size),pos -> world.setBlock(pos.x,pos.y,pos.z,Blocks.lapis_block));
+		island.generate(world);
+		
+		int lowest = 128;
+		
+		for(int x = -1; x <= 1; x++){
+			for(int z = -1; z <= 1; z++){
+				lowest = Math.min(lowest,world.getTopY(x,z,Blocks.end_stone));
+			}
+		}
+		
+		for(int x = -1; x <= 1; x++){
+			for(int z = -1; z <= 1; z++){
+				world.setBlock(x,lowest,z,Blocks.end_portal,Meta.endPortalActive);
+			}
+		}
 	}
 }
