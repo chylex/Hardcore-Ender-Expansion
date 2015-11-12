18028dcf2cf36432022cd9be50f8314e8c23ce93
chylex <info@chylex.com>
2015-11-13 00:18:50 +0100

    Push a WIP Enderman update

--- src/main/java/chylex/hee/entity/mob/EntityMobEnderman.java
+++ src/main/java/chylex/hee/entity/mob/EntityMobEnderman.java
@@ -1,7 +1,13 @@
 package chylex.hee.entity.mob;
-import java.util.List;
+import java.util.IdentityHashMap;
 import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -12,6 +18,7 @@ import net.minecraft.util.DamageSource;
 import net.minecraft.util.StatCollector;
 import net.minecraft.world.World;
 import chylex.hee.entity.GlobalMobData.IIgnoreEnderGoo;
+import chylex.hee.entity.mob.ai.AIUtil;
 import chylex.hee.init.BlockList;
 import chylex.hee.init.ItemList;
 import chylex.hee.mechanics.causatum.Causatum;
@@ -21,6 +28,8 @@ import chylex.hee.mechanics.causatum.CausatumEventHandler;
 import chylex.hee.mechanics.causatum.events.CausatumEventInstance.EventTypes;
 import chylex.hee.mechanics.misc.Baconizer;
 import chylex.hee.proxy.ModCommonProxy;
+import chylex.hee.system.ReflectionPublicizer;
+import chylex.hee.system.abstractions.BlockInfo;
 import chylex.hee.system.abstractions.util.EntitySelector;
 import chylex.hee.world.loot.PercentageLootTable;
 import chylex.hee.world.loot.info.LootMobInfo;
@@ -50,10 +59,44 @@ public class EntityMobEnderman extends EntityEnderman implements IIgnoreEnderGoo
 		drops.addLoot(BlockList.enderman_head).setChances(obj -> {
 			return new float[]{ 0.03F };
 		});
+		
+		ReflectionPublicizer.f__carriable__EntityEnderman(new IdentityHashMap<Block,Boolean>(){
+			@Override
+			public Boolean get(Object key){
+				return Boolean.FALSE;
+			}
+		});
 	}
 	
 	public EntityMobEnderman(World world){
 		super(world);
+		AIUtil.clearEntityTasks(this);
+		
+		tasks.addTask(1,new EntityAISwimming(this));
+		tasks.addTask(2,new EntityAIAttackOnCollide(this,1D,false));
+		tasks.addTask(3,new EntityAIWander(this,1D));
+		tasks.addTask(4,new EntityAIWatchClosest(this,EntityPlayer.class,8F));
+		tasks.addTask(4,new EntityAILookIdle(this));
+		
+		experienceValue = 10;
+	}
+	
+	@Override
+	protected void entityInit(){
+		super.entityInit();
+		dataWatcher.addObject(30,Short.valueOf((short)0));
+		dataWatcher.addObject(31,Byte.valueOf((byte)0));
+	}
+	
+	@Override
+	protected boolean isAIEnabled(){
+		return true;
+	}
+	
+	@Override
+	public void onLivingUpdate(){
+		super.onLivingUpdate();
+		// TODO
 	}
 	
 	@Override
@@ -90,16 +133,6 @@ public class EntityMobEnderman extends EntityEnderman implements IIgnoreEnderGoo
 	}
 
 	@Override
-	public void func_146081_a(Block carriedBlock){
-		dataWatcher.updateObject(30,Short.valueOf((short)Block.getIdFromBlock(carriedBlock)));
-	}
-
-	@Override
-	public Block func_146080_bZ(){
-		return Block.getBlockById(dataWatcher.getWatchableObjectShort(30));
-	}
-
-	@Override
 	protected String getLivingSound(){
 		return Baconizer.soundNormal(super.getLivingSound());
 	}
@@ -121,6 +154,60 @@ public class EntityMobEnderman extends EntityEnderman implements IIgnoreEnderGoo
 	
 	@Override
 	public boolean getCanSpawnHere(){
-		return super.getCanSpawnHere() && (worldObj.provider.dimensionId != 0 || !worldObj.isDaytime());
+		return super.getCanSpawnHere() && (worldObj.provider.dimensionId != 0 || worldObj.skylightSubtracted <= 5);
 	}
+	
+	/**
+	 * Replacement method for the ones that set the carried block and metadata.
+	 */
+	public void setCarrying(BlockInfo info){
+		dataWatcher.updateObject(30,Short.valueOf((short)Block.getIdFromBlock(info.block)));
+		dataWatcher.updateObject(31,info.meta);
+	}
+	
+	/**
+	 * Replacement method for the ones that return the carried block and metadata.
+	 */
+	public BlockInfo getCarrying(){
+		return new BlockInfo(Block.getBlockById(dataWatcher.getWatchableObjectShort(30)),dataWatcher.getWatchableObjectByte(31));
+	}
+	
+	/**
+	 * Replacement method for isWet() to avoid automatic damage in EntityEnderman.onLivingUpdate() and replace it with a custom system.
+	 */
+	public boolean isEndermanWet(){
+		return super.isWet();
+	}
+	
+	// Disabled methods
+	
+	@Override
+	public boolean isWet(){ return false; }
+	
+	@Override
+	public void setScreaming(boolean isScreaming){}
+	
+	@Override
+	public boolean isScreaming(){ return false; }
+
+	@Override
+	public void func_146081_a(Block carriedBlock){}
+
+	@Override
+	public Block func_146080_bZ(){ return Blocks.air; }
+	
+	@Override
+	public void setCarryingData(int data){}
+	
+	@Override
+	public int getCarryingData(){ return 0; }
+	
+	@Override
+	protected boolean teleportRandomly(){ return false; }
+	
+	@Override
+	protected boolean teleportTo(double x, double y, double z){ return false; }
+	
+	@Override
+	protected boolean teleportToEntity(Entity entity){ return false; }
 }
