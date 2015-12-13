d061f2b276ac04adbc953d3a443da46d0750a3b9
chylex <info@chylex.com>
2015-12-13 23:31:24 +0100

    Work on Knowledge Objects and their positioning

--- src/main/java/chylex/hee/mechanics/compendium/KnowledgeCategories.java
+++ src/main/java/chylex/hee/mechanics/compendium/KnowledgeCategories.java
@@ -1,16 +1,13 @@
 package chylex.hee.mechanics.compendium;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
-import chylex.hee.init.BlockList;
 import chylex.hee.mechanics.compendium.content.KnowledgeCategory;
 
 public class KnowledgeCategories{
 	public static final KnowledgeCategory
-		TEST1 = new KnowledgeCategory("ec.title.stronghold",new ItemStack(Blocks.stonebrick),KnowledgeRegistrations.TEST2),
-		TEST2 = new KnowledgeCategory("ec.title.stronghold",new ItemStack(BlockList.end_portal_frame),KnowledgeRegistrations.TEST6),
-		TEST3 = new KnowledgeCategory("ec.title.stronghold",new ItemStack(Blocks.end_stone),KnowledgeRegistrations.TEST5);
+		TEST1 = new KnowledgeCategory("ec.title.overworld",new ItemStack(Blocks.stonebrick),KnowledgeRegistrations.ENDERMAN);
 	
 	public static final KnowledgeCategory[] list = new KnowledgeCategory[]{
-		TEST1, TEST2, TEST3
+		TEST1
 	};
 }
--- src/main/java/chylex/hee/mechanics/compendium/KnowledgeRegistrations.java
+++ src/main/java/chylex/hee/mechanics/compendium/KnowledgeRegistrations.java
@@ -1,62 +1,81 @@
 package chylex.hee.mechanics.compendium;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import chylex.hee.entity.mob.EntityMobEnderman;
+import chylex.hee.entity.mob.EntityMobSilverfish;
 import chylex.hee.init.BlockList;
 import chylex.hee.init.ItemList;
 import chylex.hee.item.ItemSpecialEffects;
 import chylex.hee.mechanics.compendium.content.KnowledgeFragment;
 import chylex.hee.mechanics.compendium.content.KnowledgeObject;
-import chylex.hee.mechanics.compendium.content.fragments.FragmentText;
-import chylex.hee.mechanics.compendium.content.fragments.KnowledgeFragmentType;
 import chylex.hee.mechanics.compendium.content.objects.IObjectHolder;
 import chylex.hee.mechanics.compendium.content.objects.ObjectBlock;
 import chylex.hee.mechanics.compendium.content.objects.ObjectDummy;
+import chylex.hee.mechanics.compendium.content.objects.ObjectItem;
+import chylex.hee.mechanics.compendium.content.objects.ObjectMob;
+import chylex.hee.system.abstractions.Meta;
 
 public final class KnowledgeRegistrations{
 	public static final KnowledgeObject<? extends IObjectHolder<?>>
 		HELP = $(new ObjectDummy("HALP",new ItemStack(ItemList.special_effects,1,ItemSpecialEffects.questionMark)),"ec.title.help"),
 		
-		TEST1 = $(new ObjectBlock(BlockList.obsidian_falling)),
-		TEST2 = $(new ObjectBlock(BlockList.decomposition_table)),
-		TEST3 = $(new ObjectBlock(BlockList.end_powder_ore)),
-		TEST4 = $(new ObjectBlock(BlockList.endium_block)),
-		TEST5 = $(new ObjectBlock(BlockList.ethereal_lantern)),
-		TEST6 = $(new ObjectBlock(BlockList.laboratory_floor));
+		ENDERMAN = $(new ObjectMob(EntityMobEnderman.class)),
+		ENDER_PEARL = $(new ObjectItem(Items.ender_pearl)),
+		ETHEREUM = $(new ObjectItem(ItemList.ethereum)),
+		ENDERMAN_HEAD = $(new ObjectBlock(BlockList.enderman_head)),
+		EYE_OF_ENDER = $(new ObjectItem(Items.ender_eye)),
+		
+		STRONGHOLD = $(new ObjectDummy("STRONGHOLD",new ItemStack(Blocks.stonebrick))),
+		DRY_VINE = $(new ObjectBlock(BlockList.dry_vine)),
+		ANCIENT_COBWEB = $(new ObjectBlock(BlockList.ancient_web)),
+		STONE_BRICK = $(new ObjectBlock(Blocks.stonebrick)),
+		STONE_BRICK_SLAB = $(new ObjectBlock(Blocks.stone_slab,Meta.slabStoneBrickBottom)),
+		STONE_BRICK_STAIRS = $(new ObjectBlock(Blocks.stone_brick_stairs)),
+		STONE_BRICK_WALL = $(new ObjectBlock(BlockList.stone_brick_wall)),
+		SILVERFISH = $(new ObjectMob(EntityMobSilverfish.class)),
+		ANCIENT_DUST = $(new ObjectItem(ItemList.ancient_dust)),
+		END_PORTAL = $(new ObjectBlock(BlockList.end_portal_frame))
+		;
 	
 	public static void initialize(){
 		HELP.setHidden().addFragments(new KnowledgeFragment[]{
-			new FragmentText(0).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(1).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(2).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(3).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(4).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(5).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(6).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(7).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(8).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(9).setType(KnowledgeFragmentType.VISIBLE)
+			
 		});
 		
-		TEST2.setParent(TEST1,0,4).addFragments(new KnowledgeFragment[]{
-			new FragmentText(20).setType(KnowledgeFragmentType.ESSENTIAL),
-			new FragmentText(21).setType(KnowledgeFragmentType.DISCOVERY)
-		});
+		ENDERMAN.setCategoryObject().setImportant();
 		
-		TEST3.setParent(TEST2,-5,3).addParentLine(0,-2).addParentLine(2,-2).addParentLine(2,-3);
+		ENDER_PEARL.setParent(ENDERMAN,-3,5).addParentLine(0,-2);
 		
-		TEST4.setParent(TEST2,3,3).setImportant().addChildLine(0,2).addFragments(new KnowledgeFragment[]{
-			new FragmentText(40).setType(KnowledgeFragmentType.ESSENTIAL),
-			new FragmentText(41).setType(KnowledgeFragmentType.DISCOVERY),
-			new FragmentText(42).setType(KnowledgeFragmentType.SECRET,10)
-		});
+		ETHEREUM.setParent(ENDERMAN,3,5).addParentLine(0,-2);
 		
-		TEST5.setParent(TEST4,0,10).addFragments(new KnowledgeFragment[]{
-			new FragmentText(50).setType(KnowledgeFragmentType.VISIBLE),
-			new FragmentText(51).setType(KnowledgeFragmentType.DISCOVERY)
-		});
+		ENDERMAN_HEAD.setParent(ENDERMAN,0,3);
 		
-		TEST6.setParent(TEST4,3,8).addParentLine(-3,0).addFragments(new KnowledgeFragment[]{
+		EYE_OF_ENDER.setParent(ENDER_PEARL,3,3).addParent(ETHEREUM).addParentLine(0,-3);
+		
+		STRONGHOLD.setParent(EYE_OF_ENDER,0,4).setImportant();
+		
+		DRY_VINE.setParent(STRONGHOLD,0,7);
+		
+		ANCIENT_COBWEB.setParent(STRONGHOLD,-2,7).addParentLine(0,-2).addParentLine(1,-3).addParentLine(2,-3).addChildLine(0,2);
+		
+		STONE_BRICK.setParent(STRONGHOLD,-6,2).addParentLine(2,-2);
+		
+		STONE_BRICK_SLAB.setParent(STONE_BRICK,-5,2).addParentLine(2,0).addParentLine(2,-1).addParentLine(3,-2);
+		
+		STONE_BRICK_STAIRS.setParent(STONE_BRICK,-5,4).addParentLine(2,0).addParentLine(2,-3).addParentLine(3,-4);
+		
+		STONE_BRICK_WALL.setParent(STONE_BRICK,-5,6).addParentLine(2,0).addParentLine(2,-5).addParentLine(3,-6);
+		
+		SILVERFISH.setParent(STONE_BRICK,0,5).addChildLine(0,2);
+		
+		ANCIENT_DUST.setParent(SILVERFISH,2,4).addParent(ANCIENT_COBWEB);
+		
+		END_PORTAL.setParent(STRONGHOLD,4,10).addParentLine(0,-9).addParentLine(-1,-10);
+		
+		/*TEST6.setParent(TEST4,3,8).addParentLine(-3,0).addFragments(new KnowledgeFragment[]{
 			new FragmentText(60).setType(KnowledgeFragmentType.HINT)
-		});
+		});*/
 	}
 	
 	private static <T extends IObjectHolder<?>> KnowledgeObject<T> $(T holder){
