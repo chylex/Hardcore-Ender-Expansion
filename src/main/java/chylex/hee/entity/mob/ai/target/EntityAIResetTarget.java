fe75ff2664d0546a9f5d3de01c4243f24fc0a979
chylex <info@chylex.com>
2015-12-22 16:22:25 +0100

    Add conditions to target resetting AI

--- src/main/java/chylex/hee/entity/mob/ai/target/EntityAIResetTarget.java
+++ src/main/java/chylex/hee/entity/mob/ai/target/EntityAIResetTarget.java
@@ -1,12 +1,17 @@
 package chylex.hee.entity.mob.ai.target;
+import java.util.function.Predicate;
 import net.minecraft.entity.EntityCreature;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAITarget;
+import net.minecraft.entity.player.EntityPlayer;
 import chylex.hee.entity.mob.ai.base.EntityAIAbstractTarget;
 
 /**
  * Special task used by {@link EntityAIAbstractTarget} to determine when to stop attacking a target.
  */
 public class EntityAIResetTarget extends EntityAITarget{
+	private Predicate<EntityLivingBase> conditions = target -> target.isEntityAlive();
+	
 	public EntityAIResetTarget(EntityCreature owner){
 		super(owner,false,false);
 	}
@@ -16,9 +21,51 @@ public class EntityAIResetTarget extends EntityAITarget{
 		return false;
 	}
 	
+	public EntityAIResetTarget addCondition(Predicate<EntityLivingBase> condition){
+		conditions = conditions.and(condition);
+		return this;
+	}
+	
+	public EntityAIResetTarget stopIfTooFar(){
+		return addCondition(target -> {
+			double maxDist = getTargetDistance();
+			return taskOwner.getDistanceSqToEntity(target) > maxDist*maxDist;
+		});
+	}
+	
+	public EntityAIResetTarget stopIfTooFar(final double maxDistance){
+		final double maxDistSq = maxDistance*maxDistance;
+		return addCondition(target -> taskOwner.getDistanceSqToEntity(target) > maxDistSq);
+	}
+	
+	public EntityAIResetTarget stopIfLostSight(final int ticks){
+		return addCondition(new Predicate<EntityLivingBase>(){
+			private int limiter;
+			
+			@Override
+			public boolean test(EntityLivingBase target){
+				if (taskOwner.getEntitySenses().canSee(target))limiter = 0;
+				else if (++limiter >= ticks)return false;
+				
+				return true;
+			}
+		});
+	}
+	
+	public EntityAIResetTarget stopIfCreative(){
+		return addCondition(target -> !(target instanceof EntityPlayer) || !((EntityPlayer)target).capabilities.isCreativeMode);
+	}
+	
+	public EntityAIResetTarget setVanilla(boolean checkSight){
+		stopIfTooFar();
+		if (checkSight)stopIfLostSight(60);
+		stopIfCreative();
+		return this;
+	}
+	
 	@Override
 	public boolean continueExecuting(){
-		// TODO
-		return taskOwner.getAttackTarget() != null && taskOwner.getAttackTarget().isEntityAlive();//super.continueExecuting();
+		EntityLivingBase target = taskOwner.getAttackTarget();
+		return target != null && conditions.test(target);
 	}
 }
