8e62e2faf9bc33bd53eacbf8ea4bb441277b016d
chylex <info@chylex.com>
2016-01-06 00:44:22 +0100

    Make the empty enum set a singleton

--- src/main/java/chylex/hee/system/collections/EmptyEnumSet.java
+++ src/main/java/chylex/hee/system/collections/EmptyEnumSet.java
@@ -4,11 +4,13 @@ import java.util.EnumSet;
 public final class EmptyEnumSet{
 	private enum UselessEnum{}
 	
+	private static final EnumSet iAmTheOneAndOnly = EnumSet.noneOf(UselessEnum.class);
+	
 	/**
 	 * This works, don't fucking touch it, and don't do any writes into the EnumSet because things will explode.
 	 */
 	public static <T extends Enum<T>> EnumSet<T> get(){
-		return (EnumSet<T>)EnumSet.noneOf(UselessEnum.class);
+		return iAmTheOneAndOnly;
 	}
 	
 	private EmptyEnumSet(){}
