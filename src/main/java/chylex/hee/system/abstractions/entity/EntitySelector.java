74e9363c5f5b94facfffa963d596258a3359a386
chylex <info@chylex.com>
2016-01-13 17:25:35 +0100

    Add closest entity selection methods to EntitySelector

--- src/main/java/chylex/hee/system/abstractions/entity/EntitySelector.java
+++ src/main/java/chylex/hee/system/abstractions/entity/EntitySelector.java
@@ -2,6 +2,7 @@ package chylex.hee.system.abstractions.entity;
 import java.util.Collections;
 import java.util.List;
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
@@ -11,6 +12,7 @@ import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.ServerConfigurationManager;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
+import chylex.hee.system.collections.CollectionUtil;
 
 public final class EntitySelector{
 	public static @Nonnull List<EntityPlayerMP> players(){
@@ -44,6 +46,22 @@ public final class EntitySelector{
 	
 	public static @Nonnull <T extends Entity> List<T> type(World world, Class<T> extending, AxisAlignedBB boundingBox){
 		return world.getEntitiesWithinAABB(extending,boundingBox);
+	}
+	
+	public static @Nullable <T extends Entity> T closest(Entity targetEntity, List<T> entities){
+		return CollectionUtil.min(entities,entity -> entity.isDead || entity == targetEntity ? Double.MAX_VALUE : targetEntity.getDistanceSqToEntity(entity)).orElse(null);
+	}
+	
+	public static @Nullable <T extends Entity> T closest(Entity targetEntity, AxisAlignedBB boundingBox, IEntitySelectorMethod<T> selector){
+		return closest(targetEntity,selector.select(targetEntity.worldObj,boundingBox));
+	}
+	
+	public static @Nullable <T extends Entity> T closest(Entity targetEntity, Class<T> extending, AxisAlignedBB boundingBox){
+		return closest(targetEntity,type(targetEntity.worldObj,extending,boundingBox));
+	}
+	
+	public static interface IEntitySelectorMethod<T extends Entity>{
+		List<T> select(World world, AxisAlignedBB boundingBox);
 	}
 	
 	private EntitySelector(){}
