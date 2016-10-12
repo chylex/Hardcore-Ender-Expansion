a577badc35e08cc1b1b274f4e4bd4aff38b77b01
chylex <info@chylex.com>
2016-10-12 20:22:27 +0200

    Remove the Forge version update batch file

--- ..UPDATE.bat
+++
@@ -1,4 +0,0 @@
-@xcopy "eclipse/.metadata" "eclipse/.metadata.back" /E /H /I /Q
-@start /B gradlew.bat setupDecompWorkspace eclipse
-@rmdir /S /Q "eclipse/.metadata"
-@ren "eclipse/.metadata.back" ".metadata"
